8b75c0d501cc841d9d35b2b5cc7a466a9d6e0f3a
MarvsPSison <163713145+MarvsPSison@users.noreply.github.com>
2024-03-17 19:45:22 +0800

    Update Program.cs

--- oop/Program.cs
+++ oop/Program.cs
@@ -1,34 +1,81 @@
-﻿class Car {
+﻿class Sorcerer //Base class (parent) 
+ {
 
     //git
     //Attributes
-    public string Model;
-    public string Make;
-    public int Year;
+    public string Ability;
+    public string Grade;
+    public string Name;
+
+    //Encapsulation
+    public string AbilityNew
+    {
+        get { return Ability; }
+        set { Ability = value; }
+    }
+
+    public string GradeNew
+    {
+        get { return Grade; }
+        set { Grade = value; }
+    }
+
+    public string NameNew
+    {
+        get { return Name; }
+        set { Name = value; }
+    }
 
     //Constructor
-    public Car(string model, string make, int year) {
-        Model = model;
-        Make = make;
-        Year = year;
+    public Sorcerer(string ability, string grade, string name) 
+    {
+        Ability = ability;
+        Grade = grade;
+        Name = name;
     }
 
     //Methods
-    public void Drive() {
-        Console.WriteLine("The car is now running.");
+    public virtual void Rank() 
+    {
+        Console.WriteLine("Congratulations! You have been promoted to Special Grade Sorcerer.");
     }
 
-    public void Stop() {
-        Console.WriteLine("The car has stopped.");
+    public virtual void Race() 
+    {
+        Console.WriteLine("You use your powers for criminal purposes, therefore you are a Curse User.");
     }
 }
+//Inheritance: Creating a subclass for Sorcerer
+class NonSorcerer : Sorcerer 
+    {
+//Constructor for the sub-class
+public NonSorcerer(string ability, string grade, string name): base(ability, grade, name)
+{
+}
 
-class Program {
-    static void Main(string[] args) {
-        Car myCar = new Car("Toyota", "Corolla", 2023);
-        Console.WriteLine($"Model: {myCar.Model}, Make: {myCar.Make}, Year: {myCar.Year}");
-        myCar.Drive();
-        myCar.Stop();
+//Polymorphism: Override Rank Method
+public override void Rank()
+{
+    Console.WriteLine("You don't possess any curse energy, but rankings are based on merits. Therefore you are promoted to Grade 4 Sorcerer!");
+}
+public override void Race()
+{
+    Console.WriteLine("You were born without any curse energy, therefore you are classified as a Non-Sorcerer.");
+}
+    }
 
+class Program 
+{
+    static void Main(string[] args) 
+    {
+        Sorcerer jjSorcerer = new Sorcerer("Cursed Spirit Manipulation", "Special Grade", "Suguru Geto");
+        Console.WriteLine($"Ability: {jjSorcerer.Ability}, Grade: {jjSorcerer.Grade}, Name: {jjSorcerer.Name}");
+        jjSorcerer.Rank();
+        jjSorcerer.Race();
+
+        NonSorcerer diffRace = new NonSorcerer("Heaven Restrictrion", "Grade 4", "Maki Zenin");
+        Console.WriteLine($"Ability: {diffRace.Ability}, Grade: {diffRace.Grade}, Name: {diffRace.Name}");
+        diffRace.Rank();
+        diffRace.Race();
     }
 }
