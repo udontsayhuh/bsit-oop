5dc6ace63a95fad40c2ac66840119fa819f05e58
RegGail <federez.reginagail.asprec@gmail.com>
2024-03-18 01:28:46 +0800

    Submission to OOP, Modified to have Encapsulation, Inheritance, Abstraction, and Polymorphism incorporated in the code
    Regina Gail Federez
    BSIT 2-2

--- oop/Program.cs
+++ oop/Program.cs
@@ -1,36 +1,82 @@
-﻿class Car {
-    //test
-    //git
-    //Attributes
-    public string Model;
-    public string Make;
-    public int Year;
+﻿// Abstract base class for Car
+abstract class Car {
+    // Attributes
+    private string model;
+    private string make;
+    private int year;
 
-    //Constructor
+    // Encapsulated properties
+    public string Model {
+        get { return model; }
+        set { model = value; }
+    }
+
+    public string Make {
+        get { return make; }
+        set { make = value; }
+    }
+
+    public int Year {
+        get { return year; }
+        set { year = value; }
+    }
+
+    // Constructor
     public Car(string model, string make, int year) {
         Model = model;
         Make = make;
         Year = year;
     }
 
-    //Methods
-    public void Drive() {
-        Console.WriteLine("The car is now running.");
+    // Abstract method Drive
+    public abstract void Drive();
+
+    // Abstract method Stop
+    public abstract void Stop();
+}
+
+// Derived class ElectricCar
+class ElectricCar : Car {
+    // Constructor
+    public ElectricCar(string model, string make, int year) : base(model, make, year) {
     }
 
-    public void Stop() {
-        Console.WriteLine("The car has stopped.");
+    // Implementing Drive method
+    public override void Drive() {
+        Console.WriteLine("The electric car is now running.");
+    }
+
+    // Implementing Stop method
+    public override void Stop() {
+        Console.WriteLine("The electric car has stopped.");
+    }
+}
+// Derived class ManualCar
+class ManualCar : Car {
+    // Constructor
+    public ManualCar(string model, string make, int year) : base(model, make, year) {
+    }
+
+    // Implementing Drive method
+    public override void Drive() {
+        Console.WriteLine("The manual car is now running.");
+    }
+
+    // Implementing Stop method
+    public override void Stop() {
+        Console.WriteLine("The manual car has stopped.");
     }
 }
 
 class Program {
     static void Main(string[] args) {
-        Car myCar = new Car("Toyota", "Corolla", 2023);
-        Console.WriteLine($"Model: {myCar.Model}, Make: {myCar.Make}, Year: {myCar.Year}");
-        myCar.Drive();
-        myCar.Stop();
-
+        Car manuCar = new ManualCar("Suzuki", "Dzire", 2008);
+        Car electCar = new ElectricCar("Tesla", "Model S", 2023);
+        Console.WriteLine($"Model: {manuCar.Model}, Make: {manuCar.Make}, Year: {manuCar.Year}");
+        manuCar.Drive();
+        manuCar.Stop();
+        Console.WriteLine($"Model: {electCar.Model}, Make: {electCar.Make}, Year: {electCar.Year}");
+        electCar.Drive();
+        electCar.Stop();
     }
 }
-
-
