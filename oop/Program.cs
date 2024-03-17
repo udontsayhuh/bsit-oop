3aa1b103552d0f35f4a8f3d8ed33e4e4f6876480
LennoxMacadangdang <163654511+LennoxMacadangdang@users.noreply.github.com>
2024-03-17 18:30:32 +0800

    Update Program.cs

--- oop/Program.cs
+++ oop/Program.cs
@@ -1,6 +1,6 @@
-﻿class Car {
-    //test
-    //git
+﻿﻿class Car {
+
+    //Encapsulation: Making attributes private and providing public
     //Attributes
     public string Model;
     public string Make;
@@ -13,22 +13,57 @@
         Year = year;
     }
 
-    //Methods
-    public void Drive() {
-        Console.WriteLine("The car is now running.");
+    // Abstraction: Implementing methods that can be overridden by subclasses.
+    public virtual void Start() {
+        Console.WriteLine("The Car is now started.");
     }
 
-    public void Stop() {
-        Console.WriteLine("The car has stopped.");
+    public virtual void Rev() {
+        Console.WriteLine("The Car has started revving to 6,000 RPM (VTEC)");
+    }
+    public virtual void Stop()
+    {
+        Console.WriteLine("The Car has now stopped after running out of gas");
+    }
+}
+
+// Inheritance: Creating a subclass that inherits from Car.
+class ElectricCar : Car
+{
+    public ElectricCar(string model, string make, int year) : base(model, make, year)
+    {
+    }
+
+    // Polymorphism: Overriding methods from the base class.
+    public override void Start()
+    {
+        Console.WriteLine("The Electric Car is now running silently.");
+    }
+
+    public override void Rev()
+    {
+        Console.WriteLine("The Electric car has revved to 15,000 RPM");
+    }
+    public override void Stop()
+    {
+        Console.WriteLine("The Electric car has now stopped after running out of charge");
     }
 }
 
 class Program {
     static void Main(string[] args) {
-        Car myCar = new Car("Toyota", "Corolla", 2023);
-        Console.WriteLine($"Model: {myCar.Model}, Make: {myCar.Make}, Year: {myCar.Year}");
-        myCar.Drive();
+        Car myCar = new Car("Honda", "Civic", 2000);
+        Console.WriteLine($"\nModel: {myCar.Model}, Make: {myCar.Make}, Year: {myCar.Year}");
+        myCar.Start();
+        myCar.Rev();
         myCar.Stop();
 
+
+        ElectricCar myElectricCar = new ElectricCar("Tesla", "Model S", 2024);
+        Console.WriteLine($"\nModel: {myElectricCar.Model}, Make: {myElectricCar.Make}, Year: {myElectricCar.Year}");
+        myElectricCar.Start();
+        myElectricCar.Rev();
+        myElectricCar.Stop();
     }
 }
+//Lennox Macadangdang BSIT 2-1
