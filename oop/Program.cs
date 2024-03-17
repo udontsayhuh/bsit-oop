7bb458c53ad43c943ccf253210c0d4e22ca79866
Christelle Anne <christelleannesdacapias@iskolarngbayan.pup.edu.ph>
2024-03-17 22:22:05 +0800

    Here is my assignment po

--- oop/Program.cs
+++ oop/Program.cs
@@ -1,34 +1,83 @@
-﻿class Car {
-    //test
-    //git
+﻿using System;
+
+class Vehicle
+{
     //Attributes
     public string Model;
     public string Make;
     public int Year;
 
     //Constructor
-    public Car(string model, string make, int year) {
+    public Vehicle(string model, string make, int year)
+    {
         Model = model;
         Make = make;
         Year = year;
     }
 
     //Methods
-    public void Drive() {
+    public virtual void Drive()
+    {
         Console.WriteLine("The car is now running.");
     }
 
-    public void Stop() {
-        Console.WriteLine("The car has stopped.");
+    public virtual void Stop()
+    {
+        Console.WriteLine("The car has stopped.\n");
     }
 }
 
-class Program {
-    static void Main(string[] args) {
-        Car myCar = new Car("Toyota", "Corolla", 2023);
+    //Abstraction
+abstract class Transportation
+{
+    public abstract void Travel();
+}
+
+    //Inheritance from class vehicle
+class Car : Vehicle
+{
+    public Car(string model, string make, int year) : base(model, make, year)
+    {
+    }
+        //Polymorphism
+    public override void Drive()
+    {
+        Console.WriteLine("Our red car is now running.\n");
+    }
+}
+
+class Bus : Transportation
+{
+    public override void Travel()
+    {
+        Console.WriteLine("Bus: PITX/MOA");
+    }
+}
+
+class EJeep : Transportation
+{
+    public override void Travel()
+    {
+        Console.WriteLine("EJeep: Litex/Commonwealth Market");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Vehicle myVehicle = new Vehicle("Toyota", "Corolla", 2023);
+        Vehicle myCar = new Car("Honda", "Civic", 2024);
+        Transportation myBus = new Bus();
+        Transportation myEJeep = new EJeep();
+
+        Console.WriteLine($"Model: {myVehicle.Model}, Make: {myVehicle.Make}, Year: {myVehicle.Year}");
+        myVehicle.Drive();
+        myVehicle.Stop();
+
         Console.WriteLine($"Model: {myCar.Model}, Make: {myCar.Make}, Year: {myCar.Year}");
         myCar.Drive();
-        myCar.Stop();
 
+        myBus.Travel();
+        myEJeep.Travel();
     }
 }
