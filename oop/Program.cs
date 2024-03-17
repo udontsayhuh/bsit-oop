8e0e1b983071cc0792b1ee7a4b4299dd36d36583
Kristine Mae Cleofas <163546627+Krxtinezx@users.noreply.github.com>
2024-03-18 00:41:05 +0800

    Update Program-cleofas.cs

--- oop/Program.cs
+++ oop/Program.cs
@@ -1,34 +1,93 @@
-﻿class Car {
+using System;
+class Vehicle { // base class (parent)
     //test
     //git
     //Attributes
     public string Model;
     public string Make;
     public int Year;
-
+     
     //Constructor
-    public Car(string model, string make, int year) {
+    public Vehicle(string model, string make, int year) {
         Model = model;
         Make = make;
         Year = year;
     }
 
     //Methods
-    public void Drive() {
+    public virtual void Drive() {
         Console.WriteLine("The car is now running.");
     }
 
-    public void Stop() {
+    public virtual void Stop() {
         Console.WriteLine("The car has stopped.");
     }
 }
+    //Abstraction
+    abstract class TravelMode
+    {
+    public string Mode;
+    public TravelMode(string mode)
+    {
+        Mode = mode;
+    }
+    public abstract void Travel();
+    }
+
+    // Derived class / Inheritance (child)
+class Car: Vehicle {
+    public Car(string model, string make, int year) : base (model, make, year)
+    {
+        
+    }
+    //Polymorphism
+    public override void Drive()
+    {    Console.WriteLine("Our car is now running.");
+     
+    }
+}
+    
+class Tricycle : TravelMode
+{
+    public Tricycle() : base ("Tricycle")
+    {
+    
+    }
+    public override void Travel()
+    {
+        Console.WriteLine($"Tricycle: SM FAIRVIEW / ROBINSONS / FAIRVIEW TERRACES");
+    }
+}
+
+class Jeep : TravelMode
+{
+    public Jeep() : base ("Jeep")
+    {
+
+    }
+    public override void Travel()
+    {
+        Console.WriteLine($"Jeep: ROBINSONS / SM FAIRVIEW IKOT-LOOB / LAGRO LOOB-DULO");
+    }
+}
+
 
 class Program {
     static void Main(string[] args) {
-        Car myCar = new Car("Toyota", "Corolla", 2023);
+        Vehicle myVehicle = new Vehicle("Toyota", "Corolla", 2023);
+        Vehicle myCar = new Car("Ford", "Tesla", 2024);
+        TravelMode myTricycle = new Tricycle();
+        TravelMode myJeep = new Jeep();
+
+        Console.WriteLine($"Model: {myVehicle.Model}, Make: {myVehicle.Make}, Year: {myVehicle.Year}");
+        myVehicle.Drive();
+        myVehicle.Stop();
+        
         Console.WriteLine($"Model: {myCar.Model}, Make: {myCar.Make}, Year: {myCar.Year}");
         myCar.Drive();
-        myCar.Stop();
+
+        myTricycle.Travel();
+        myJeep.Travel();
 
     }
 }
