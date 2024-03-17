a1410efae367bc7a7233c9d0309aef42c6171a71
mxla14 <163645368+mxla14@users.noreply.github.com>
2024-03-17 16:29:20 +0800

    Update Program.cs

--- oop/Program.cs
+++ oop/Program.cs
@@ -1,10 +1,20 @@
-﻿class Car {
+//abstraction and encapsulation
+abstract class Vehicle
+{
+    public abstract void Drive();
+    public abstract void Stop()
+    public abstract string Model {get; set;} //getting and setting the info based on the main (program)
+    public abstract string Make {get; set;}
+    public abstract int Year {get; set} 
+
+    //inheritance
+class Car : Vehicle
 
     //git
     //Attributes
-    public string Model;
-    public string Make;
-    public int Year;
+    public override string Model {get; set;}
+    public override string Make {get; set;}
+    public override int Year {get; set;}
 
     //Constructor
     public Car(string model, string make, int year) {
@@ -14,21 +24,29 @@
     }
 
     //Methods
-    public void Drive() {
+    public override void Drive() 
+    {
         Console.WriteLine("The car is now running.");
     }
 
-    public void Stop() {
+    public override void Stop() {
         Console.WriteLine("The car has stopped.");
     }
 }
 
 class Program {
     static void Main(string[] args) {
-        Car myCar = new Car("Toyota", "Corolla", 2023);
-        Console.WriteLine($"Model: {myCar.Model}, Make: {myCar.Make}, Year: {myCar.Year}");
-        myCar.Drive();
-        myCar.Stop();
+        //accessing the car and putting details about car
+        Vehicle carnum1 = new Car("Toyota", "Corolla", 2023);
+        Vehicle carnum2 = new Car("Audi","R8", 2006);
+
+        carnum1.Drive();
+        carnum1.Stop();
+        Console.WriteLine($"Model: {carnum1.Model}, Make: {carnum1.Make}, Year: {carnum1.Year};
+
+         carnum2.Drive();
+        carnum2.Stop();
+        Console.WriteLine($"Model: {carnum2.Model}, Make: {carnum2.Make}, Year: {carnum2.Year};
 
     }
 }
