89b1b1e075442b8510fc0304dd8ab7bd3da00464
Yuan Evangelista <163483561+Chiliseu@users.noreply.github.com>
2024-03-17 19:54:14 +0800

    Abstraction, inheritance, polymorphs
    added the last 3 pillars

--- oop/Program.cs
+++ oop/Program.cs
@@ -33,10 +33,55 @@
     public void Stop() {
         Console.WriteLine("The car has stopped.");
     }
-
+    //POLYMORPHISM
     public virtual void show_information()
     {
         Console,WriteLine($"Model: {Model}, Make: {Make}, Year: {Year}, Vehicle Information Number: {VIN}");
+    }
+}
+
+//ABSTRACTION
+abstract class electric_car : Car //INHERITANCE
+{
+    //UNIQUE ATTRIBUTE
+    public int chargeTime;
+
+    public electric_car(string model, string make, int year, string V_I_N, int ChargeTime) : base(model, make, year, V_I_N)
+    {
+        ChargeTime = chargeTime;
+    }
+    
+    //POLYMORPHISM
+    public override voide show_information()
+    {
+        base.show_information();
+        Console.WriteLine($"Charging Time: {chargeTime}");
+    }
+    public abstract void Charge_battery();
+}
+
+class Vintage_Car : Car // INHERITANCE
+{
+    //UNIQUE ATTRIBUTE
+    public bool HaveTouchscreenDisplay;
+
+    //CONSTRUCTOR
+    public Vintage_Car(string model, string make, int year, string V_I_N, bool haveTouchScreenDisplay) : base(model, make, year, V_I_N)
+    {
+        HaveTouchscreenDisplay = haveTouchscreenDisplay; 
+    }
+
+        //METHODS
+    public void playMusic()
+    {
+        Console.WriteLine("The car is now playing music 🎵 🎵 🎵");
+    }
+    
+    //POLYMORPHISM
+    public override void show_information()
+    {
+        base.show_information();
+        Console.WriteLine($"Do you have Touch Screen Display?: {HaveTouchscreenDispaly});
     }
 }
 
