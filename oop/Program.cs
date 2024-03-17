f178b000b953ba01c8231170b40de7ac3735a56f
Cedrick Owen Gale Keh <163647850+CedrickOwenGaleKeh@users.noreply.github.com>
2024-03-17 19:25:52 +0800

    Update Program.cs

--- oop/Program.cs
+++ oop/Program.cs
@@ -1,34 +1,108 @@
-﻿class Car {
+//  class for vehicles
+abstract class Vehicle
+{
+    // Encapsulated attributes
+    private string Model { get; }
+    private string Make { get; }
+    private int Year { get; }
 
-    //git
-    //Attributes
-    public string Model;
-    public string Make;
-    public int Year;
-
-    //Constructor
-    public Car(string model, string make, int year) {
+    // Constructor
+    public Vehicle(string model, string make, int year)
+    {
         Model = model;
         Make = make;
         Year = year;
     }
 
-    //Methods
-    public void Drive() {
-        Console.WriteLine("The car is now running.");
+    // get model
+    public string GetModel()
+    {
+        return Model;
     }
 
-    public void Stop() {
-        Console.WriteLine("The car has stopped.");
+    // get make
+    public string GetMake()
+    {
+        return Make;
+    }
+
+    // get year
+    public int GetYear()
+    {
+        return Year;
+    }
+
+    // Method for driving
+    public abstract void Drive();
+
+    // Method for stopping
+    public abstract void Stop();
+}
+
+// Class Tricycle inherits from Vehicle
+class Tricycle : Vehicle
+{
+    // Constructor
+    public Tricycle(string model, string make, int year) : base(model, make, year) { }
+
+    // Implementation of Drive method
+    public override void Drive()
+    {
+        Console.WriteLine("The tricycle is now running.");
+    }
+
+    // Implementation of Stop method
+    public override void Stop()
+    {
+        Console.WriteLine("The tricycle has stopped.");
     }
 }
 
-class Program {
-    static void Main(string[] args) {
-        Car myCar = new Car("Toyota", "Corolla", 2023);
-        Console.WriteLine($"Model: {myCar.Model}, Make: {myCar.Make}, Year: {myCar.Year}");
-        myCar.Drive();
-        myCar.Stop();
+// Class motorcycle inherits from Vehicle
+class Motorcycle : Vehicle
+{
+    // Constructor
+    public Motorcycle(string model, string make, int year) : base(model, make, year) { }
 
+    // Implementation of Drive method
+    public override void Drive()
+    {
+        Console.WriteLine("The PCX is now running and flying.");
+    }
+
+    // Implementation of Stop method
+    public override void Stop()
+    {
+        Console.WriteLine("The PCX has stopped and get a ticket from an enforcer.");
     }
 }
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Using polymorphism to create a Tricycle object
+        Vehicle myTricycle = new Tricycle("XYZ", "ABC", 2020);
+
+        // Using abstraction to access attributes
+        Console.WriteLine($"Tricycle - Model: {myTricycle.GetModel()}, Make: {myTricycle.GetMake()}, Year: {myTricycle.GetYear()}");
+
+        // Invoking Drive and Stop methods for Tricycle
+        myTricycle.Drive();
+        myTricycle.Stop();
+
+        // Using polymorphism to create a Motorcycle object
+        Vehicle myMotorcycle = new Motorcycle("HONDA", "PCX 10,000cc", 2019);
+
+        // Using abstraction to access attributes
+        Console.WriteLine($"Motorcycle - Model: {myMotorcycle.GetModel()}, Make: {myMotorcycle.GetMake()}, Year: {myMotorcycle.GetYear()}");
+
+        // Invoking Drive and Stop methods for Motorcycle
+        myMotorcycle.Drive();
+        myMotorcycle.Stop();
+       
+
+    }
+
+}
+
