a8279e1ffbc5e4a923c18383e27e021780d2317e
Mark Regie A. Magtangob <110184282+Markxregie@users.noreply.github.com>
2024-03-18 02:02:06 +0800

    Program.cs

--- oop/Program.cs
+++ oop/Program.cs
@@ -1,34 +1,72 @@
-﻿class Car {
-    //test
-    //git
-    //Attributes
-    public string Model;
-    public string Make;
-    public int Year;
+using System;
 
-    //Constructor
-    public Car(string model, string make, int year) {
+abstract class Car
+{
+    private string Model;
+    private string Make;
+    private int Year;
+
+    public Car(string model, string make, int year)
+    {
         Model = model;
         Make = make;
         Year = year;
     }
 
-    //Methods
-    public void Drive() {
+    public string GetModel()
+    {
+        return Model;
+    }
+
+    public string GetMake()
+    {
+        return Make;
+    }
+
+    public int GetYear()
+    {
+        return Year;
+    }
+
+
+    public abstract void DisplayDetails();
+
+    public void Drive()
+    {
         Console.WriteLine("The car is now running.");
     }
 
-    public void Stop() {
+    public void Stop()
+    {
         Console.WriteLine("The car has stopped.");
     }
 }
 
-class Program {
-    static void Main(string[] args) {
-        Car myCar = new Car("Toyota", "Corolla", 2023);
-        Console.WriteLine($"Model: {myCar.Model}, Make: {myCar.Make}, Year: {myCar.Year}");
-        myCar.Drive();
-        myCar.Stop();
+class ElectricCar : Car
+{
+    private int Price;
 
+    public ElectricCar(string model, string make, int year, int price) : base(model, make, year)
+    {
+        Price = price;
+    }
+
+    public override void DisplayDetails()
+    {
+        Console.WriteLine($"Model: {GetModel()}, Make: {GetMake()}, Year: {GetYear()}, Price: {Price}");
     }
 }
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ElectricCar newCar = new ElectricCar("Tesla", "Cybertruck", 2024, 100000);
+        newCar.DisplayDetails();
+
+        newCar.Drive();
+        newCar.Stop();
+    }
+}
+
+
