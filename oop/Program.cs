a8c594cfbf0a1c734b0c3b537358edb24438c3a2
anjobette <163548141+anjobette@users.noreply.github.com>
2024-03-18 00:47:49 +0800

    Update Program.cs

--- oop/Program.cs
+++ oop/Program.cs
@@ -1,34 +1,75 @@
-﻿class Car {
-    //test
-    //git
+//Encapsulation used in base class Vehicle
+//Abstraction to define common attributes and methods
+abstract class Vehicle 
+{
     //Attributes
-    public string Model;
-    public string Make;
-    public int Year;
+    public string Model { get; set; }
+    public string Make { get; set; }
+    public int Year { get; set; }
 
     //Constructor
-    public Car(string model, string make, int year) {
+    public Vehicle(string model, string make, int year) 
+    {
         Model = model;
         Make = make;
         Year = year;
     }
 
     //Methods
-    public void Drive() {
-        Console.WriteLine("The car is now running.");
+    public abstract void Drive();
+    public abstract void Stop();
+}
+
+//Inheritance from class Vehicle and Polymorphism using override modifier
+class Car: Vehicle
+{
+    //Constructor
+    public Car(string model, string make, int year) : base(model, make, year)
+    {
     }
 
-    public void Stop() {
-        Console.WriteLine("The car has stopped.");
+    public override void Drive()
+    {
+        Console.WriteLine($"The {Model} {Make} is now running.");
+    }
+
+    public override void Stop()
+    {
+        Console.WriteLine($"The {Model} {Make} has stopped.\n");
     }
 }
 
-class Program {
-    static void Main(string[] args) {
-        Car myCar = new Car("Toyota", "Corolla", 2023);
+//Inheritance from class Vehicle and Polymorphism using override modifier
+class Van: Vehicle
+{
+    //Constructor
+    public Van(string model, string make, int year) : base(model, make, year)
+    {
+    }
+
+    public override void Drive()
+    {
+        Console.WriteLine($"The {Model} {Make} is now running.");
+    }
+
+    public override void Stop()
+    {
+        Console.WriteLine($"The {Model} {Make} has stopped.\n");
+    }
+}
+
+class Program 
+{
+    static void Main(string[] args) 
+    {
+        Vehicle myCar = new Car("Toyota", "Corolla", 2023);
         Console.WriteLine($"Model: {myCar.Model}, Make: {myCar.Make}, Year: {myCar.Year}");
         myCar.Drive();
         myCar.Stop();
 
+        Vehicle myVan = new Van("Kia", "Carnival", 2024);
+        Console.WriteLine($"Model: {myVan.Model}, Make: {myVan.Make}, Year: {myVan.Year}");
+        myVan.Drive();
+        myVan.Stop();
     }
 }
