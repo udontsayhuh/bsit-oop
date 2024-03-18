d229deff811cc181f1d0cba4f6125db4df3aca82
Zaina Ghazi <zaina3ghazi@gmail.com>
2024-03-18 12:05:24 +0800

    OOP ASSIGNMENT#1

--- oop/Program.cs
+++ oop/Program.cs
@@ -1,34 +1,70 @@
-﻿class Car {
-    //test
-    //git
-    //Attributes
-    public string Model;
-    public string Make;
-    public int Year;
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
-    //Constructor
-    public Car(string model, string make, int year) {
+abstract class Car
+{
+    private string Model;
+    private string Make;
+    private int Year;
+
+    public Car(string model, string make, int year)
+    {
         Model = model;
         Make = make;
         Year = year;
     }
 
-    //Methods
-    public void Drive() {
+    public string GetModel()
+    {
+        return Model;
+    }
+    public string GetMake()
+    {
+        return Make;
+    }
+
+    public int GetYear()
+    {
+        return Year;
+    }
+
+    //display car details using abstraction
+    public abstract void DisplayDetails();
+
+    public void Drive()
+    {
         Console.WriteLine("The car is now running.");
     }
 
-    public void Stop() {
+    public void Stop()
+    {
         Console.WriteLine("The car has stopped.");
     }
 }
 
-class Program {
-    static void Main(string[] args) {
-        Car myCar = new Car("Toyota", "Corolla", 2023);
-        Console.WriteLine($"Model: {myCar.Model}, Make: {myCar.Make}, Year: {myCar.Year}");
-        myCar.Drive();
-        myCar.Stop();
+class SUVCar : Car
+{
+    private int Price;
 
+    public SUVCar(string model, string make, int year, int price) : base(model, make, year)
+    {
+        Price = price;
+    }
+
+    public override void DisplayDetails() 
+    { 
+        Console.WriteLine($"Model: {GetModel()}, Make: {GetMake()}, Year: {GetYear()}, Price: {Price}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SUVCar newCar = new SUVCar("Mitsubishi", "Xpander", 2024, 1068000);
+        newCar.DisplayDetails();
+
+        newCar.Drive();
+        newCar.Stop();
     }
 }
