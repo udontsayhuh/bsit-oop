2bffeca3db5e6bfea6c73c1fa5b36c32fe9019f7
Markloydlicay <163738993+Markloydlicay@users.noreply.github.com>
2024-03-18 21:49:50 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,34 +1,57 @@
-class Car {
-    //test
-    //git
+class Music
+{
+    //Encapsulation: Making attributes public
     //Attributes
-    public string Model;
-    public string Make;
-    public int Year;
+    public string Song;
+    public string Artist;
 
     //Constructor
-    public Car(string model, string make, int year) {
-        Model = model;
-        Make = make;
-        Year = year;
+    public Music(string song, string artist)
+    {
+        Song = song;
+        Artist = artist;
     }
-
+    // Abstraction: Implementing methods that can be overridden by subclasses.
     //Methods
-    public void Drive() {
-        Console.WriteLine("The car is now running.");
+    public virtual void Play()
+    {
+        Console.WriteLine("Relax now, The music is playing.\n");
     }
 
-    public void Stop() {
-        Console.WriteLine("The car has stopped.");
+    public virtual void Stop()
+    {
+        Console.WriteLine("The music has stopped playing.\n");
+    }
+}//Inheritance: 
+class Instrumental: Music
+{
+    //Constructor
+    public Instrumental(string song, string artist) : base(song, artist) { }
+
+    //Polymorphism: Ovveriding method
+    public override void Play()
+    { 
+        Console.WriteLine("The Instrumental Music is now playing.\n");
+    }
+    public override void Stop()
+    {
+        Console.WriteLine("The Instrumental Music has now stopped.\n");
     }
 }
+   
+class Program
+{
+    static void Main(string[] args)
+    {
+        Music myPlaylist = new Music("Para sa Streets", "Hev Abi");
+        Console.WriteLine($"Song: {myPlaylist.Song}, Artist: {myPlaylist.Artist}");
+        myPlaylist.Play();
+        myPlaylist.Stop();
 
-class Program {
-    static void Main(string[] args) {
-        Car myCar = new Car("Toyota", "Corolla", 2023);
-        Console.WriteLine($"Model: {myCar.Model}, Make: {myCar.Make}, Year: {myCar.Year}");
-        myCar.Drive();
-        myCar.Stop();
+        Instrumental myInstrumental = new Instrumental("A Thousand Years", "The Piano Guys");
+        Console.WriteLine($"Song: {myInstrumental.Song}, Artist: {myInstrumental.Artist}");
+        myInstrumental.Play();
+        myInstrumental.Stop();
 
     }
 }
