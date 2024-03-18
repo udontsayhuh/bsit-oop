c9ae9db8d91f372c47d97eb62d150dffc7672315
Rudj <162955108+rudjj@users.noreply.github.com>
2024-03-18 17:15:24 +0800

    Assignment#1

--- Program.cs
+++ Program.cs
@@ -1,70 +1,62 @@
-﻿using System;
-using System.Security.Cryptography.X509Certificates;
+using System;
 
 abstract class Car
 {
-    private string Model;
-    private string Make;
-    private int Year;
+    private string model;
+    private string make;
+    private int year;
 
     public Car(string model, string make, int year)
     {
+
         Model = model;
         Make = make;
         Year = year;
     }
-
-    public string GetModel()
+    public string Model
     {
-        return Model;
-    }
-    public string GetMake()
-    {
-        return Make;
+        get { return model; }
+        set { model = value; }
     }
 
-    public int GetYear()
+    public string Make
     {
-        return Year;
+        get { return make; }
+        set { make = value; }
     }
 
-    //display car details using abstraction
-    public abstract void DisplayDetails();
+    public int Year
+    {
+        get { return year; }
+        set { year = value; }
+    }
 
     public void Drive()
     {
-        Console.WriteLine("The car is now running.");
+        Console.WriteLine("The sports car is now running.");
     }
 
     public void Stop()
     {
-        Console.WriteLine("The car has stopped.");
+        Console.WriteLine("The sports car has stopped.");
     }
 }
 
-class SUVCar : Car
+class SportsCar : Car
 {
-    private int Price;
-
-    public SUVCar(string model, string make, int year, int price) : base(model, make, year)
+    public SportsCar(string model, string make, int year) : base(model, make, year)
     {
-        Price = price;
     }
-
-    public override void DisplayDetails() 
-    { 
-        Console.WriteLine($"Model: {GetModel()}, Make: {GetMake()}, Year: {GetYear()}, Price: {Price}");
-    }
-}
+} 
 
 class Program
 {
     static void Main(string[] args)
     {
-        SUVCar newCar = new SUVCar("Mitsubishi", "Xpander", 2024, 1068000);
-        newCar.DisplayDetails();
 
-        newCar.Drive();
-        newCar.Stop();
+        SportsCar mySportsCar = new SportsCar("Porsche", "911 GT3 RS", 2003);
+        Console.WriteLine($"Model: {mySportsCar.Model}, Make: {mySportsCar.Make}, Year: {mySportsCar.Year}");
+        mySportsCar.Drive();
+        mySportsCar.Stop();
     }
 }
