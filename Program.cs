8fed06b5f0c89a32634aae6a064e1923ba48f36e
BenJr23 <139469706+BenJr23@users.noreply.github.com>
2024-03-19 02:27:45 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,34 +1,70 @@
-class Car {
-    //test
-    //git
-    //Attributes
-    public string Model;
-    public string Make;
-    public int Year;
+using System;
 
-    //Constructor
-    public Car(string model, string make, int year) {
+abstract class Jet
+{
+    private string Model;
+    private string Make;
+    private int Year;
+
+    public Jet(string model, string make, int year)
+    {
         Model = model;
         Make = make;
         Year = year;
     }
 
-    //Methods
-    public void Drive() {
-        Console.WriteLine("The car is now running.");
+    public string GetModel()
+    {
+        return Model;
     }
 
-    public void Stop() {
-        Console.WriteLine("The car has stopped.");
+    public string GetMake()
+    {
+        return Make;
+    }
+
+    public int GetYear()
+    {
+        return Year;
+    }
+
+
+    public abstract void DisplayDetails();
+
+    public void Flying()
+    {
+        Console.WriteLine("The jet is flying");
+    }
+
+    public void Stop()
+    {
+        Console.WriteLine("The Jet has landed.");
     }
 }
 
-class Program {
-    static void Main(string[] args) {
-        Car myCar = new Car("Toyota", "Corolla", 2023);
-        Console.WriteLine($"Model: {myCar.Model}, Make: {myCar.Make}, Year: {myCar.Year}");
-        myCar.Drive();
-        myCar.Stop();
+class Plane : Jet
+{
+    private int Price;
 
+    public Plane(string model, string make, int year, int price) : base(model, make, year)
+    {
+        Price = price;
+    }
+
+    public override void DisplayDetails()
+    {
+        Console.WriteLine($"Model: {GetModel()}, Make: {GetMake()}, Year: {GetYear()}, Price: {Price}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Plane newJet = new Plane("Embraer/FMA", "CBA 123 Vector", 1990, 15000000);
+        newJet.DisplayDetails();
+
+        newJet.Flying();
+        newJet.Stop();
     }
 }
