cd9e3c0b64ed26c0276f9fa83cb135a0dbfe2e18
sammy <115720937+smsrsz04@users.noreply.github.com>
2024-03-18 15:56:19 +0800

    updated

--- oop/Program.cs
+++ oop/Program.cs
@@ -1,34 +1,71 @@
-﻿class Car {
-    //test
-    //git
-    //Attributes
-    public string Model;
-    public string Make;
-    public int Year;
+﻿
+namespace oop
+{
+    class Car
+    {
+        private string model;
+        private string modelmake;
+        private int year;
 
-    //Constructor
-    public Car(string model, string make, int year) {
-        Model = model;
-        Make = make;
-        Year = year;
+
+
+        //getters and setters
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public string Modelmake
+        {
+            get { return modelmake; }
+            set { modelmake = value; }
+        }
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+        public Car() { }
+        public Car(string Model, string ModelMake, int Year)
+        {
+            model = Model;
+            modelmake = Modelmake;
+            year = Year;
+        }
     }
-
-    //Methods
-    public void Drive() {
-        Console.WriteLine("The car is now running.");
+    class Horn : Car
+    {
+        public void peep()
+        {
+            if (Model == "Ford Fortuner")
+            {
+                Console.WriteLine($"Model: {Model}\nModelMake: {Modelmake}\nYear: {Year}\n");
+                Console.WriteLine($"The horn on the {Modelmake}) was peep!");
+            }
+            else if (Model == "Honda civic")
+            {
+                Console.WriteLine($"Model: {Model}\nModelMake: {Modelmake}\nYear: {Year}\n");
+                Console.WriteLine($"The horn on the {Modelmake} was broom!");
+            }
+        }
     }
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Car Car1 = new Car();
+            Car1.Model = "Ford Fortuner";
+            Car1.Modelmake = "Coupe";
+            Car1.Year = 2024;
 
-    public void Stop() {
-        Console.WriteLine("The car has stopped.");
-    }
-}
+            Horn horn1 = new Horn();
+            horn1.Model = Car1.Model;
+            horn1.Modelmake = Car1.Modelmake;
+            horn1.Year = 2024;
 
-class Program {
-    static void Main(string[] args) {
-        Car myCar = new Car("Toyota", "Corolla", 2023);
-        Console.WriteLine($"Model: {myCar.Model}, Make: {myCar.Make}, Year: {myCar.Year}");
-        myCar.Drive();
-        myCar.Stop();
-
+            horn1.peep();
+            Console.ReadLine();
+        }
     }
 }
