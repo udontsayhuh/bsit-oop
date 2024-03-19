44812403a9cc545df31b818e74919c52ca026b60
itsmiyukimharie <163590615+itsmiyukimharie@users.noreply.github.com>
2024-03-19 15:13:54 +0800

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,34 +1,79 @@
-class Car {
-    //test
-    //git
-    //Attributes
-    public string Model;
-    public string Make;
-    public int Year;
+abstract class Vehicle { // implementing abstraction
 
-    //Constructor
-    public Car(string model, string make, int year) {
-        Model = model;
-        Make = make;
-        Year = year;
+    private string Model; // implementing encapsulation
+    private string Make;
+    private int Year;
+    private string Color;
+
+    public void getInfo(string model, string make, int year) { // implementing polymorphism
+        this.Model = model;
+        this.Make = make;
+        this.Year = year;
     }
 
-    //Methods
-    public void Drive() {
+    public void showInfo() { // implementing polymorphism
+        Console.WriteLine($"Model: {Model}, Make: {Make}, Year: {Year}");
+
+    }
+    public void getColor(string color) { // implementing polymorphism
+        this.Color = color;
+    }
+    
+    public void showColor(){ // implementing polymorphism
+        Console.WriteLine("Color: " + Color);
+    }
+     public void Drive() { // implementing polymorphism
         Console.WriteLine("The car is now running.");
     }
 
-    public void Stop() {
+    public void Stop() { // implementing polymorphism
         Console.WriteLine("The car has stopped.");
     }
+    public abstract void engine(); // declaring an abstract method 
+
+}
+
+class Car : Vehicle { // implementing inheritance using colon(:)
+     public override void engine() { // overriding an abstract method
+        Console.WriteLine("brrrr");
+     }
+}
+
+class Motor : Vehicle {
+    public override void engine() { // overriding an abstract method
+        Console.WriteLine("broom broom");
+     }
 }
 
 class Program {
-    static void Main(string[] args) {
-        Car myCar = new Car("Toyota", "Corolla", 2023);
-        Console.WriteLine($"Model: {myCar.Model}, Make: {myCar.Make}, Year: {myCar.Year}");
+    public static void Main(string[] args) {
+        Car myCar = new Car(); // create new object
+        Motor myMotor = new Motor(); // create new object
+
+        myCar.getInfo("Toyota", "Corolla", 2023);
+        myCar.getColor("Black");
+        myMotor.getInfo("Honda", "Goldwing", 2024);
+        myMotor.getColor("Pink");
+
+        myCar.showInfo();
+        myCar.showColor();
+        myCar.engine();
         myCar.Drive();
         myCar.Stop();
 
+        Console.WriteLine("\n------------------------------------------\n");
+
+        myMotor.showInfo();
+        myMotor.showColor();
+        myMotor.engine();
+        myMotor.Drive();
+        myMotor.Stop();
+        
+        Console.ReadKey();
     }
 }
+
+/*
+Submitted By: Miyuki Mharie C. Parocha
+Year/Section: BSIT 2-2
+*/
