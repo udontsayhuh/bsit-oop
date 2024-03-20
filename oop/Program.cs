52c11c53506aa8695f474772ad56c83a00caeb0a
Mark Cardeño <markjuscardeno13@gmail.com>
2024-03-20 10:32:06 +0800

    Main Function

--- oop/Program.cs
+++ oop/Program.cs
@@ -1,68 +1,18 @@
-﻿// Main Fuction
+﻿using oop;
+
+// Main Method
 class Program {
     static void Main(string[] args) {
-        Player p1 = new Player1("Zynx", 19, "Fanny");
-        Console.WriteLine(Player1: p1.PlayerName);
+        //Player 1
+        Player p1 = new Player1("Zynx", 20, "Fanny");
+        p1.Information();
         p1.Status();
         p1.Do();
 
-    }
-}
+        //Player 2
+        Player p2 = new Player1("Cyphra", 19, "Fanny");
 
-// Family Class
-abstract class Player
-{
-    //Player Information
-    private string playerName, heroName;
-    private int playerAge;
+        //Player 3
 
-
-    public abstract void Status();
-    public abstract void Do();
-    public string onlineStatus = "Online";
-    public string offlineStatus = "Offline";
-    public string inGame = "In Game";
-    public string inQueue = "In Queue";
-
-    //Getters and Setters 
-    public string PlayerName
-    {
-        get { return playerName; }
-        set { playerName = value; }
-    }
-
-    public int PlayerAge
-    {
-        get { return playerAge; }
-        set { playerAge = value; }
-    }
-    public string HeroName
-    {
-        get { return heroName; }
-        set { heroName = value; }
-    }
-
-    public Player(string playerName, int playerAge, string heroName)
-    {
-        PlayerName = playerName;
-        PlayerAge = playerAge;
-        HeroName = heroName;
-    }
-
-}
-
-class Player1 : Player
-{
-    
-    public Player1(string playerName, int playerAge, string heroName) : base(playerName, playerAge, heroName){}
-    // Status
-    public override void Status()
-    {
-        Console.WriteLine("Player 1 : " + onlineStatus);
-    }
-    // Do
-    public override void Do()
-    {
-        Console.WriteLine("Player 1 is " + inQueue);
     }
 }
