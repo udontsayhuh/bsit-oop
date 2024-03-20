feba63b3628b917e21b1330902af7d9c7699e78e
Edzra Macas <EdzraM7@gmail.com>
2024-03-20 17:41:10 +0800

    Concepts of OOP implementation.

--- oop/Program.cs
+++ oop/Program.cs
@@ -1,34 +1,61 @@
-﻿class Car {
+﻿using System;
+
+class Car
+{
     //test
     //git
-    //Attributes
+    // Attributes
     public string Model;
-    public string Make;
+    private string make;
     public int Year;
 
-    //Constructor
-    public Car(string model, string make, int year) {
+    // Encapsulation
+    public string Make
+    {
+        get { return make; }
+        set { make = value; }
+    }
+    // Constructors
+    public Car(string model, string make, int year)
+    {
         Model = model;
         Make = make;
         Year = year;
     }
 
-    //Methods
-    public void Drive() {
+    // Methods
+    // Abstraction
+    public virtual void Drive()
+    {
         Console.WriteLine("The car is now running.");
     }
-
-    public void Stop() {
+    public virtual void Stop()
+    {
         Console.WriteLine("The car has stopped.");
     }
 }
 
-class Program {
-    static void Main(string[] args) {
+// Inheritance
+class SportsCar : Car
+{
+    public SportsCar(string model, string make, int year) : base(model, make, year)
+    {
+    }
+
+    //Polymorphism
+    public override void Drive()
+    {
+        Console.WriteLine("The sports car is now running fast.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
         Car myCar = new Car("Toyota", "Corolla", 2023);
         Console.WriteLine($"Model: {myCar.Model}, Make: {myCar.Make}, Year: {myCar.Year}");
         myCar.Drive();
         myCar.Stop();
-        Console.WriteLine("Test");
     }
 }
