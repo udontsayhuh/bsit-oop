0393dde620b42faaaebcc57e4f7b97bbbaef582a
Mark Cardeño <markjuscardeno13@gmail.com>
2024-03-20 10:49:12 +0800

    comments

--- oop/Program.cs
+++ oop/Program.cs
@@ -6,15 +6,16 @@ class Program {
         //Player 1
         // Input: Name, Gender, Age, Favorite Hero, Favorite Hero Winrate, 
         Player p1 = new Player1("Zynx", "Male", 20, "Fanny", 63.95); 
-        p1.Information();
-        p1.Status();
-        p1.Do();
+        p1.Information(); // print the Information
+        p1.Status(); // online or offline
+        p1.Do(); // in game or in queue
+
         //Player 2
         // Input: Name, Gender, Age, Favorite Hero, Favorite Hero Winrate, 
         Player p2 = new Player1("Cyphra", "Female", 19, "Lylia", 55.67);
-        p1.Information();
-        p1.Status();
-        p1.Do();
+        p1.Information(); // print the Information
+        p1.Status(); // online or offline
+        p1.Do(); // in game or in queue
 
     }
 }
