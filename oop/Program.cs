af298c15cc5f1069bea58acc5de04dc8cbaf2a9e
Mark Cardeño <markjuscardeno13@gmail.com>
2024-03-20 11:21:50 +0800

    add player number

--- oop/Program.cs
+++ oop/Program.cs
@@ -5,7 +5,7 @@ class Program {
     static void Main(string[] args) {
         //Player 1
         // Input: Name, Gender, Age, Favorite Hero, Favorite Hero Winrate, 
-        Player p1 = new Player1("Zynx", "Male", 20, "Fanny", 63.95); 
+        Player p1 = new Player1(1, "Zynx", "Male", 20, "Fanny", 63.95); 
         p1.Information(); // print the Information
         p1.Status(); // online or offline
         p1.Do(); // in game or in queue
@@ -14,7 +14,7 @@ class Program {
 
         //Player 2
         // Input: Name, Gender, Age, Favorite Hero, Favorite Hero Winrate, 
-        Player p2 = new Player2("Cyphra", "Female", 19, "Lylia", 55.67);
+        Player p2 = new Player2(2, "Cyphra", "Female", 19, "Lylia", 55.67);
         p2.Information(); // print the Information
         p2.Status(); // online or offline
         p2.Do(); // in game or in queue
