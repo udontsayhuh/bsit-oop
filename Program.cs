fe057422c214752175726a5f373c8c6ddef89341
Ranyeelll <163998069+Ranyeelll@users.noreply.github.com>
2024-03-20 22:31:04 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,19 +1,19 @@
-class Car {
-    //test
-    //git
-    //Attributes
-    public string Model;
-    public string Make;
-    public int Year;
+using System;
 
-    //Constructor
+class Car {
+    // Encapsulation
+    private string Model;
+    private string Make;
+    private int Year;
+    
+    // Constructor
     public Car(string model, string make, int year) {
         Model = model;
         Make = make;
         Year = year;
     }
 
-    //Methods
+    // Encapsulation
     public void Drive() {
         Console.WriteLine("The car is now running.");
     }
@@ -21,14 +21,37 @@ class Car {
     public void Stop() {
         Console.WriteLine("The car has stopped.");
     }
+
+    // Abstraction
+    public abstract void StartEngine();
+}
+    // Inheritance
+class SportsCar : Car {
+    public int Horsepower;
+
+    // Constructor for SportsCar
+    public SportsCar(string model, string make, int year, int horsepower)
+        : base(model, make, year) {
+        Horsepower = horsepower;
+    }
+
+    // Polymorphism
+    public override void Drive() {
+        Console.WriteLine($"The sports car accelerates with {Horsepower} horsepower!!!");
+    }
+
+    // Abstraction     
+    public override void StartEngine() {
+        Console.WriteLine("Sports car engine started.");
+    }
 }
 
 class Program {
     static void Main(string[] args) {
-        Car myCar = new Car("Toyota", "Corolla", 2023);
-        Console.WriteLine($"Model: {myCar.Model}, Make: {myCar.Make}, Year: {myCar.Year}");
-        myCar.Drive();
-        myCar.Stop();
-
+        SportsCar mySportsCar = new SportsCar("Chevrolet", "Corvette", 2024, 495);
+        Console.WriteLine($"Model: {mySportsCar.Model}, Make: {mySportsCar.Make}, Year: {mySportsCar.Year}");
+        mySportsCar.StartEngine(); 
+        mySportsCar.Drive(); 
+        mySportsCar.Stop();
     }
 }
