e337b7a877cbe9cf21d6155b3483233946a77798
Joel-EstradaJr <163813334+Joel-EstradaJr@users.noreply.github.com>
2024-03-20 13:25:15 +0800

    Update Program.cs
    encap

--- Program.cs
+++ Program.cs
@@ -1,34 +1,29 @@
-class Car {
-    //test
-    //git
-    //Attributes
-    public string Model;
-    public string Make;
-    public int Year;
+// Encapsulation
+using System;
 
-    //Constructor
-    public Car(string model, string make, int year) {
-        Model = model;
-        Make = make;
-        Year = year;
+class Car
+{
+    private int year;
+    private string model;
+    private string make;
+
+    public Car(int year, string model, string make)
+    {
+        this.year = year;
+        this.model = model;
+        this.make = make;
     }
 
-    //Methods
-    public void Drive() {
-        Console.WriteLine("The car is now running.");
-    }
-
-    public void Stop() {
-        Console.WriteLine("The car has stopped.");
+    public void Display()
+    {
+        Console.WriteLine($"Car details:\n{model}(model)\n{make}(make)\n{year}(year)")
     }
 }
-
-class Program {
-    static void Main(string[] args) {
-        Car myCar = new Car("Toyota", "Corolla", 2023);
-        Console.WriteLine($"Model: {myCar.Model}, Make: {myCar.Make}, Year: {myCar.Year}");
-        myCar.Drive();
-        myCar.Stop();
-
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car car1 = new Car(1997, "Space gear", "Mitsubishi");
+        Console.ReadKey();
     }
 }
