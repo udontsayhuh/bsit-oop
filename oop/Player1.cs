5c3add9a8a021ae3789861f63f981b81c875cf9e
Mark Cardeño <markjuscardeno13@gmail.com>
2024-03-20 10:30:42 +0800

    Player 1

---
+++ oop/Player1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop
+{
+    internal class Player1 : Player
+    {
+        public Player1(string playerName, int playerAge, string faveHeroName) : base(playerName, playerAge, faveHeroName) { }
+        // Overring Status from Player
+        public override void Status()
+        {
+            Console.WriteLine("Status: " + onlineStatus);
+        }
+        // Overring Do from Player 
+        public override void Do()
+        {
+            Console.WriteLine("Player 1 is " + inQueue);
+        }
+    }
+}
