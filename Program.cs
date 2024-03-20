8b391a611c2b7e177afbb39e25a4ad3bd5ba50f3
Pilosopiya <163721648+Pilosopiya@users.noreply.github.com>
2024-03-20 10:07:14 +0800

    Update Program.cs 1

--- Program.cs
+++ Program.cs
@@ -1,18 +1,31 @@
-class Car {
-    //test
-    //git
-    //Attributes
-    public string Model;
-    public string Make;
-    public int Year;
+using System;
 
-    //Constructor
-    public Car(string model, string make, int year) {
+// Abstract class Vehicle
+abstract class Vehicle 
+
+    { // Declaring an abstract class called Vehicle
+    public string Model { get; set; }
+    public string Make { get; set; }
+    public int Year { get; set; }
+
+    public abstract void Drive();
+    }
+
+class Car : Vehicle {
+    // Constructor
+    public Car(string model, string make, int year) 
+    {
         Model = model;
         Make = make;
         Year = year;
     }
+    
+ public override void Drive() {
+    Console.WriteLine("The car is now running.");}
 
+    public void Stop() {
+        Console.WriteLine("The car has stopped."); }
+    }
     //Methods
     public void Drive() {
         Console.WriteLine("The car is now running.");
