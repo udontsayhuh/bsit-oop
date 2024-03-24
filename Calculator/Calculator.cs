052cb6d92800f93464785779779a2cf6b258c69a
Marc Cedric Mayuga <mayuga.marccedricc@gmail.com>
2024-03-24 20:05:18 +0800

    not yet done~

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -1,15 +1,68 @@
 ﻿//mrccdricmyg
 
 using System;
+using System.Runtime.CompilerServices;
+
 
 class Input
-{
-    public static void get_num()
-    {
-        Console.WriteLine("Enter an integer...");
-        string num1 = Console.ReadLine();
+{   
+
+    public static string get_input()
+    {   while (true)
+        {
+            return "string";
+        }
+   
     }
+
+    public static float get_num()
+    {   
+        while (true)
+        {
+            Console.WriteLine("Enter an integer...");
+            string input = Console.ReadLine();
+
+            // Attempt to parse the input as a float
+            if (!float.TryParse(input, out float result))
+            {
+                Console.WriteLine("The input is not numerical.");
+                
+            }
+            else
+            {
+                float num1 = Convert.ToSingle(input);
+                return num1;
+            }
+        }
+    }
+
+    public static float Calculate(float num1)
+    {
+        while (true) {
+            Console.WriteLine("Enter an operator (+, -, *, /)...");
+            string input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "+":
+                    float result = num1 + (Input.get_num());
+                    return result;
+                case "-":
+                    float result2 = num1 - (Input.get_num());
+                    return result2;
+                case "*":
+                    float result3 = num1 * (Input.get_num());
+                    return result3;
+                case "/":
+                    float result4 = num1 / (Input.get_num());
+                    return result4;
+
+            }
+        }
+    }
+
 }
+
 
 class Operator : Input
 {
@@ -20,6 +73,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Input.get_num();
+        float result = Input.Calculate(Input.get_num());
+
+        Console.WriteLine(result);
     }
 }
