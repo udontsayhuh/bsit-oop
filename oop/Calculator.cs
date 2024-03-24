86f37f3d9f088e055bcaf216af48fd6b48a2ec76
SidneyRamirez <155166018+AbsorbingYendis@users.noreply.github.com>
2024-03-24 17:01:16 +0800

    Update Calculator.cs

--- oop/Calculator.cs
+++ oop/Calculator.cs
@@ -9,13 +9,14 @@ class Calculator
             char operation;
             double num1; 
             double num2;
+            double num3;
 
             Console.WriteLine("Welcome to The Calculator");
 
             Console.Write("Enter First Number:");
             if (!double.TryParse(Console.ReadLine(), out num1))
             {
-                Console.WriteLine("Invalid input. Please enter a valid numerical value.");
+                Console.WriteLine("Invalid input. Please enter a valid number value!.");
                 continue;
             }
             
@@ -24,7 +25,14 @@ class Calculator
                 Console.Write("Enter Second number:");
             if (!double.TryParse(Console.ReadLine(), out num2))
             {
-                Console.WriteLine("Invalid input. Please enter a valid numerical value.");
+                Console.WriteLine("Invalid input. Please enter a valid number value!.");
+                continue;
+            }
+
+            Console.Write("Enter Third number:");
+            if (!double.TryParse(Console.ReadLine(), out num3))
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number value!.");
                 continue;
             }
 
@@ -44,19 +52,19 @@ class Calculator
             switch (operation)
             {
                 case '+':
-                    result = num1 + num2;
+                    result = num1 + num2 + num3;
                     Console.WriteLine("Addition:" + result);
                     break;
                 case '-':
-                    result = num1 - num2;
+                    result = num1 - num2 - num3;
                     Console.WriteLine("Subtraction:" + result);
                     break;
                 case '*':
-                    result = num1 * num2;
+                    result = num1 * num2 * num3;
                     Console.WriteLine("Multiplication:" + result);
                     break;
                 case '/':
-                    result = num1 / num2;
+                    result = num1 / num2 / num3;
                     Console.WriteLine("Division:" + result);
                     break;
             }
@@ -69,12 +77,9 @@ class Calculator
             Console.WriteLine();
             if (choice != 'Y')
             {
-                Console.WriteLine("Leaving...");
                 break;
-
-
-
             }
+  
     }
 }
     }
