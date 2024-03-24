d5c30295d938460f11eb13a02d5ebb94c2d8c985
Marc Cedric Mayuga <mayuga.marccedricc@gmail.com>
2024-03-24 18:13:17 +0800

    commit

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -1,1 +1,25 @@
-﻿//calculator by mrccdrcmyg
+﻿//mrccdricmyg
+
+using System;
+
+class Input
+{
+    public static void get_num()
+    {
+        Console.WriteLine("Enter an integer...");
+        string num1 = Console.ReadLine();
+    }
+}
+
+class Operator : Input
+{
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Input.get_num();
+    }
+}
