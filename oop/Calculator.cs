b785c6a5468c7e83bb221bd78617a421dded885b
Marc Cedric Mayuga <mayuga.marccedricc@gmail.com>
2024-03-24 16:14:24 +0800

    ciaociao

--- oop/Calculator.cs
+++ oop/Calculator.cs
@@ -1,1 +1,24 @@
 //calculator.cs mrccdrcmyg
+
+class Input
+{
+    public string operand;
+    public float num1;
+    public float num2;
+
+    public Input (float num1, string operand, float num2)
+    {
+        this.num1 = num1;
+        this.num2 = num2;   
+        this.operand = operand;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Input newInput = new Input("test", 1, 2);
+    }
+}
+    
