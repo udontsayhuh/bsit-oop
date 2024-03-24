a2583f63746f5afd94fbb7d5bc8a60284d412ff7
Kirstine Cerezo <cerezokirstine@gmail.com>
2024-03-24 19:02:42 +0800

    try

---
+++ oop/Assignment 2-Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a Number: ");
+    }
+}
+namespace oop
+{
+    internal class Calculator
+    {
+
+    }
+
+    
+}
