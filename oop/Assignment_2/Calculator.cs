b88584c877fb0245f5b8690d5afd7d9a4c3e8821
Mark Cardeño <markjuscardeno13@gmail.com>
2024-03-25 19:01:16 +0800

    adding folder for assignment 2

---
+++ oop/Assignment_2/Calculator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop.Assignment_2
+{
+    internal class Calculator
+    {
+        private int num1, num2;
+        public int sum, diff, prod, quo;
+
+        public int Num1
+        {
+            get { return num1; }
+            set { num1 = value; }
+        }
+        public int Num2
+        {
+            get { return num2; }
+            set { num2 = value; }
+        }
+    }
+
+    class Operation : Calculator
+    {
+
+        public int add(int num1, int num2)
+        {
+            sum = num1 + num2;
+            return sum;
+        }
+        public int sub(int num1, int num2)
+        {
+            diff = num1 - num2;
+            return diff;
+        }
+        public int mul(int num1, int num2)
+        {
+            prod = num1 * num2;
+            return prod;
+        }
+        public int div(int num1, int num2)
+        {
+            quo = num1 / num2;
+            return quo;
+        }
+    }
+}
---
+++ oop/Assignment_2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop.Assignment_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char op;
+            Operation calc = new Operation();
+            Console.WriteLine("Calculator");
+            // Input first number
+            Console.WriteLine("Enter your first number: ");
+            calc.Num1 = Convert.ToInt32(Console.ReadLine());
+
+            // Input second number
+            Console.WriteLine("Enter your first number: ");
+            calc.Num2 = Convert.ToInt32(Console.ReadLine());
+
+            // Choosing operation
+            Console.WriteLine("Enter Operation\n: [+] Addition\n[-] Subtraction\n[*] Multiplication");
+            Console.Write("=> ");
+            op = Convert.ToChar(Console.ReadLine());
+            char choice;
+            switch (op)
+            {
+                case '+':
+                    Console.WriteLine("= " + calc.add(calc.Num1, calc.Num2));
+                    break;
+                case '-':
+                    Console.WriteLine("= " + calc.sub(calc.Num1, calc.Num2));
+                    break;
+                case '*':
+                    Console.WriteLine("= " + calc.mul(calc.Num1, calc.Num2));
+                    break;
+                case '/':
+                    Console.WriteLine("= " + calc.div(calc.Num1, calc.Num2));
+                    break;
+                default:
+                    Console.WriteLine("Invalid Operation");
+                    break;
+            }
+
+
+        }
+    }
+}
