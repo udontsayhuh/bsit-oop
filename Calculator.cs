caef9083963dab5fa72cc046e93fd9d727ac945c
Zaina-Ghazi <163220958+Zaina-Ghazi@users.noreply.github.com>
2024-04-06 05:46:12 +0800

    Update Calculator.cs

--- Calculator.cs
+++ Calculator.cs
@@ -5,7 +5,7 @@ namespace c_sharp_Calculator
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             string value;
             do
