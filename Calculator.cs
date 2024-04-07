e0f2be64b495d4e68a9354de1bcd1c792b8c99b2
Midoshin <153992065+MeDoughSheen@users.noreply.github.com>
2024-04-08 00:49:58 +0800

    Update Calculator.cs
    Calculator.cs Activity 2

--- Calculator.cs
+++ Calculator.cs
@@ -1,169 +1,162 @@
 using System;
-using System.Globalization;
+using System.Collections.Generic;
 
-//Abstraction :3
-//Will share the TERMINATE function
-abstract class Terminator {
-
-    //Method
-    public void Terminate()
+class Calculator
+{
+    static void Main()
     {
-        Console.WriteLine("\nCalculator Terminated!");
-        Environment.Exit(0);
-    }
-
-}
-
-//Inheritance :3
-//The following subclasses inherits the TERMINATE function
-class Input1 : Terminator
-{
-
-
-}
-
-class Input2 : Terminator
-{
-
-
-}
-
-class Input3 : Terminator
-{
-   
-
-}
-
-class Input4 : Terminator
-{
-   
-
-}
-
-
-class Calcu {
-    static void Main(string[] args) { 
-        Input1 term1 = new Input1();
-        Input2 term2 = new Input2();
-        Input3 term3 = new Input3();
-        Input4 term4 = new Input4();
-        while (true)
+        // Starting Loop
+        while (true) 
         {
-        //Phase 1:
-        Console.Write("\nEnter a number                : ");
-        string input1 = Console.ReadLine();
+            List<double> numbers = new List<double>();
+            List<char> operations = new List<char>();
 
-        if (!double.TryParse(input1, out _))
-        {
-            //Failed phase 1
-            Console.Write("\nInvalid! You must enter a number ");
-            term1.Terminate();
-        }
+            bool hasPerformedCalculation = false;
 
-        //Continue to phase 2
-        else {
-            Console.Write("Enter an operator (+,-,*,/)   : ");
-        }
-
-        //Phase 2:
-        string input2 = Console.ReadLine();
-
-        if (input2 != "+" && input2 != "-" && input2 != "*" && input2 != "/")
-        {
-            //Failed phase 2
-            Console.Write("\nInvalid! You must enter one of the given operators ");
-            term2.Terminate();
-        }
-
-        //Continue to phase 3
-        else
-        {
-            char char1 = Convert.ToChar(input2);
-        }
-        
-        Console.Write("Enter another number          : ");
-        string input3 = Console.ReadLine();
-         if (!double.TryParse(input3, out _))
-        {
-            //Failed phase 3
-            Console.Write("\nInvalid! You must enter a number ");
-            term1.Terminate();
-        }
-
-        //Continue to last phase
-        else {
-            Console.Write("\n\nTotal                         : ");
-        }
-
-        //Last Phase
-        //Calculation part
-            switch(input2)
+            while (true)
             {
-                //Add
-                case "+":
-                double num1 = Convert.ToDouble(input1);
-                double num2 = Convert.ToDouble(input3);
-                Console.WriteLine(num1 + num2);
-                break;
-
-                //Subtract
-                case "-":
-                double num3 = Convert.ToDouble(input1);
-                double num4 = Convert.ToDouble(input3);
-                Console.WriteLine(num3 - num4);
-                break;
-
-                //Multiply
-                case "*":
-                double num5 = Convert.ToDouble(input1);
-                double num6 = Convert.ToDouble(input3);
-                Console.WriteLine(num5 * num6);
-                break;
-
-                //Divide
-                case "/":
-                double num7 = Convert.ToDouble(input1);
-                double num8 = Convert.ToDouble(input3);
-                Console.WriteLine(num7 / num8);
-                break;
-
-
-            }
-    
-            //Ask to repeat
-           while (true)
-            {
-                Console.Write("\n\n\n\nDo you want to try again? (Yes/No): ");
-                string choice = Console.ReadLine();
-
-                //Will start again
-                if (choice == "yes" || choice =="YES" || choice == "Yes")
+                // Input Number
+                while (true)
                 {
-                    Console.WriteLine("\n\n\n");
+                    Console.Write("\nEnter a number      : ");
+                    string input = Console.ReadLine();
+                    
+
+                    if (input == "=" && numbers.Count >= 1 && numbers.Count == operations.Count)
+                    {
+                        break;
+                    }
+
+                    double num;
+                    if (double.TryParse(input, out num))
+                    {
+                        if (num == 0 && operations.Count > 0 && operations[operations.Count - 1] == '/')
+                        {
+                            Console.WriteLine("Invalid! You cannot divide by 0, IDIOT! enter a non-zero number");
+                            continue;
+                        }
+                        numbers.Add(num);
+                        break;
+                    }
+                    else if (input == "=" && hasPerformedCalculation)
+                    {
+                        break;
+                    }
+                    else if (input == "=" && numbers.Count >= 1 && numbers.Count != operations.Count)
+                    {
+                        Console.WriteLine("Invalid! You must enter a number");
+                    }
+                    else if (input == "=")
+                    {
+                        Console.WriteLine("Invalid! You must enter a number");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid! You must enter a valid number");
+                    }
+                }
+
+                // Equals
+                if (numbers.Count >= 1 && numbers.Count == operations.Count && operations.Contains('='))
+                {
                     break;
                 }
 
-                //Will end the program
-                else if (choice == "no" || choice =="NO" || choice == "no")
+                // Input Operation
+                while (true)
                 {
-                    term4.Terminate();
+                    Console.Write("\nEnter an operation  : ");
+                    string input = Console.ReadLine();
+     
+
+                    if (input.Length == 1 && "+-*/=".Contains(input))
+                    {
+                        if (input == "=" && numbers.Count == 0) // '=' is not allowed as the first operation
+                        {
+                            Console.WriteLine("Invalid! You must enter a valid number");
+                        }
+                        else if (input == "=" && operations.Count == 0)
+                        {
+                            Console.WriteLine("Invalid! first operation must not be");
+                            continue;
+                        }
+                        else
+                        {
+                            operations.Add(input[0]);
+                        }
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid! You must enter +, -, *, /, or =");
+                    }
                 }
 
-                //Will ask again
-                else
+                // Calculations
+                if (numbers.Count >= 2 && numbers.Count == operations.Count && operations.Contains('='))
                 {
-                    Console.WriteLine("Invalid! You must enter Yes or No");
+                    double result = numbers[0];
+                    for (int i = 0; i < operations.Count; i++)
+                    {
+                        if (operations[i] == '/' && numbers[i + 1] == 0)
+                        {
+                            Console.WriteLine("Invalid! You cannot divide by 0, IDIOT! enter a non-zero number\n");
+                            hasPerformedCalculation = false;
+                            break;
+                        }
+
+                        switch (operations[i])
+                        {
+                            case '+':
+                                result += numbers[i + 1];
+                                break;
+                            case '-':
+                                result -= numbers[i + 1];
+                                break;
+                            case '*':
+                                result *= numbers[i + 1];
+                                break;
+                            case '/':
+                                result /= numbers[i + 1];
+                                break;
+                        }
+                    }
+                    Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                    Console.WriteLine($"Result              : {result}");
+                    hasPerformedCalculation = true;
+
+                    // Clear Index Values
+                    numbers.Clear();
+                    operations.Clear();
+
+                    // Try Again
+                    while (true)
+                    {
+                        Console.Write("\n\n\nDo you want to try again? (Yes/No): ");
+                        string choice = Console.ReadLine();
+
+                        // Start again
+                        if (choice.ToLower() == "yes")
+                        {
+                            Console.WriteLine("\n\n\n");
+                            break;
+                        }
+
+                        // Terminate
+                        else if (choice.ToLower() == "no")
+                        {
+                            Console.WriteLine("\nCalculator Terminated!");
+                            Environment.Exit(0);
+                        }
+
+                        // Ask
+                        else
+                        {
+                            Console.WriteLine("Invalid! You must enter 'Yes' or 'No'\n");
+                        }
+                    }
                 }
             }
         }
-        }
     }
-
-
-//*Objectives Check*
-//User Inputs : DONE
-//Invalid Input Terminations : DONE
-//Checking of Inputs : DONE
-//Calculate and Display Result : DONE
-//Start Again Option : DONE
-//Principles of OOP Applied : ABSTRACTION & INHERITANCE
+}
