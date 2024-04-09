599c76cd37d8520248e44ebf83f3ebb65e21a417
aubmil <163705907+aubmil@users.noreply.github.com>
2024-04-09 13:51:17 +0800

    cal03.cs

--- Calculator.cs
+++ Calculator.cs
@@ -6,90 +6,104 @@ namespace Calculator_Program
     {
         static void Main(string[] args)
         {
+            do
             {
+                Console.Clear();
+
+                Console.WriteLine("\n-----------------------------------------------------------------");
+                Console.WriteLine("                         Calculator Program                      ");
+                Console.WriteLine("-----------------------------------------------------------------");
+
+                double result = 0;
+                double currentNum = 0;
+                char mathOperator = ' ';
+                bool isFirstInput = true;
+
                 do
                 {
-                    double num1 = 0;
-                    double num2 = 0;
-                    double result = 0;
-
-                    Console.WriteLine("\n-----------------------------------------------------------------");
-                    Console.WriteLine("                         Calculator Program                      ");
-                    Console.WriteLine("-----------------------------------------------------------------");
-
-                    //Get the first number
-                    Console.Write("\nEnter the first number:  ");
-                    while (!double.TryParse(Console.ReadLine(), out num1))
+                    // Get the operator
+                    if (!isFirstInput)
                     {
-                        Console.WriteLine("Invalid input. Please enter a valid number.");
-                    }
-
-                    //Get second number
-                    Console.Write("Enter the second number:  ");
-                    while (!double.TryParse(Console.ReadLine(), out num2))
-                    {
-                        Console.WriteLine("Invalid input. Please eneter a valid number.");
-                    }
-
-                    //Different operators to choose 
-                    Console.WriteLine("\nPick an operation. ");
-                    Console.WriteLine("\t + : Addition ");
-                    Console.WriteLine("\t - : Sbtraction ");
-                    Console.WriteLine("\t * : Multiplication ");
-                    Console.WriteLine("\t / : Division ");
-
-                    //Asking to choose an operation to do
-                    Console.WriteLine("\nThe operation is: ");
-
-
-                    char mathOperator = ' ';
-                    while (mathOperator != '+' && mathOperator != '-' && mathOperator != '*' && mathOperator != '/')
-                    {
-                        mathOperator = Console.ReadKey().KeyChar;
-                        if (mathOperator != '+' && mathOperator != '-' && mathOperator != '*' && mathOperator != '/')
+                        Console.Write("Enter the operation (+, -, *, /, =): ");
+                        while (true)
                         {
-                            Console.WriteLine("\nInvalid operation. Please enter one of the following: +, -, *, /");
-                            Console.WriteLine("\nThe operation is: ");
-                        }
-                    }
-
-                    //Performing the calculation based on the selected mathematical operator 
-                    switch (mathOperator)
-                    {
-                        case '+':
-                            result = num1 + num2;
-                            Console.WriteLine($"\nResult: {num1} + {num2} = " + result);
-                            break;
-
-                        case '-':
-                            result = num1 - num2;
-                            Console.WriteLine($"\nResult: {num1} - {num2} = " + result);
-                            break;
-
-                        case '*':
-                            result = num1 * num2;
-                            Console.WriteLine($"\nResult: {num1} * {num2} = " + result);
-                            break;
-
-                        case '/':
-                            if (num2 != 0)
+                            mathOperator = Console.ReadKey().KeyChar;
+                            if (mathOperator != '+' && mathOperator != '-' && mathOperator != '*' && mathOperator != '/' && mathOperator != '=')
                             {
-                                result = num1 / num2;
-                                Console.WriteLine($"\nResult: {num1} / {num2} = " + result);
+                                Console.WriteLine("\n\nInvalid operation. Please enter one of the following: ");
+                                Console.WriteLine("   + (to add)");
+                                Console.WriteLine("   - (to subtract)");
+                                Console.WriteLine("   * (to multiply)");
+                                Console.WriteLine("   / (to divide) ");
+                                Console.WriteLine("   = (to get the final result)");
                             }
                             else
                             {
-                                Console.WriteLine("\nDivision by zero (0) is not allowed.");
+                                break;
                             }
-                            break;
+                        }
 
-                        default:
-                            Console.WriteLine("\nInvatid operation.");
+                        if (mathOperator == '=')
                             break;
                     }
-                    Console.WriteLine("\nDo you want to perform another calculation? (Y/N)");
-                } while (Console.ReadKey().Key == ConsoleKey.Y);
-            }
+
+                    // Get the number
+                    Console.Write("\nEnter a number: ");
+                    string input = Console.ReadLine();
+
+                    double num;
+                    while (!double.TryParse(input, out num))
+                    {
+                        Console.WriteLine("\nInvalid input. Please enter a valid number.");
+                        Console.Write("Enter a number: ");
+                        input = Console.ReadLine();
+                    }
+
+                    if (isFirstInput)
+                    {
+                        currentNum = num;
+                        isFirstInput = false;
+                        continue;
+                    }
+                    else
+                    {
+                        // Calculate the result based on the previous operator
+                        switch (mathOperator)
+                        {
+                            case '+':
+                                result = currentNum + num;
+                                break;
+                            case '-':
+                                result = currentNum - num;
+                                break;
+                            case '*':
+                                result = currentNum * num;
+                                break;
+                            case '/':
+                                if (num != 0)
+                                    result = currentNum / num;
+                                else
+                                    Console.WriteLine("\n------Division by zero (0) is not allowed.------");
+                                break;
+                            default:
+                                result = currentNum;
+                                break;
+                        }
+
+                        // Update currentNum with the new number
+                        currentNum = result;
+                    }
+
+                } while (true);
+
+                Console.WriteLine($"\n\nResult: {result}");
+
+                Console.WriteLine("\nDo you want to perform another calculation? (Y/N)");
+                string repeatInput = Console.ReadLine();
+                if (repeatInput.ToUpper() != "Y")
+                    break;
+
+            } while (true);
         }
     }
 }
