ac23e2d93b6d21b0f9cc8f714a180f4ca8f87190
Rivo Vebayo <86941434+Gknightt@users.noreply.github.com>
2024-04-13 14:35:18 +0800

    Update Calculator.cs
    First optimization

--- oop/Calculator.cs
+++ oop/Calculator.cs
@@ -15,11 +15,11 @@ namespace MyCalculator
     }
 
     // Calculator class implementing the ICalculator interface
-	// encapsulating the two input numbers
+        // encapsulating the two input numbers
     public class Calculator 
     {
-        
-		private float num1;
+
+                private float num1;
     private float num2;
 
     public float Num1 { 
@@ -29,8 +29,8 @@ namespace MyCalculator
     public float Num2 { 
         get { return num2; } 
         set { num2 = value; }
-	}
-        
+        }
+
     }
 
     class Program
@@ -38,108 +38,104 @@ namespace MyCalculator
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Basic Calculator App!");
-        
-            
-		
-		// Uses while loop if the user want to try again
-	    
-		while(true){ 	
+
+
+
+                // Uses while loop if the user want to try again
+
+                while(true){         
             Number1:
-		    Console.WriteLine("Enter a number:");
-            string num1= (Console.ReadLine());
-		    
-		// Checks if the input is numeric or non-numeric
-           if (!float.TryParse(num1, out float fnum) )
+                    Console.WriteLine("Enter a number:");
+            string Number1= (Console.ReadLine());
+
+                // Checks if the input is numeric or non-numeric
+           if (!float.TryParse(Number1, out float FirstNumber) )
             {
                 Console.WriteLine("Invalid input, Please try a valid input");
                 goto Number1;
-		    
+
             }
-		 
-		   // Using goto to use a mini loop
-		   choice:
+
+                   // Using goto to use a mini loop
+                   choice:
            Console.WriteLine("Enter operator(+,-,*,/,=) :");
-		   string choice = Console.ReadLine();
-		   
-			// Checks the input if one of the operators are inputted
-		   if(choice!= "+"&& choice != "-" && choice != "*" && choice != "/" && choice != "=")
-		   {Console.WriteLine("Invalid input.");
-				goto choice;}
-		
-		    if (choice == "=")
+                   string choice = Console.ReadLine();
+
+                        // Checks the input if one of the operators are inputted
+                   if(choice!= "+"&& choice != "-" && choice != "*" && choice != "/" && choice != "=")
+                   {Console.WriteLine("Invalid input.");
+                                goto choice;}
+
+                    if (choice == "=")
                     {
-                        Console.WriteLine($"The total is: {fnum}");
-						goto yesNo;
+                        Console.WriteLine($"The total is: {FirstNumber}");
+                                                goto yesNo;
                         break;
                     }
-			
-			num:		
-			Console.WriteLine("Enter another number:");
-			string num2=(Console.ReadLine());
-           
-		   // Checks if the input is numeric or non-numeric
-		   if (!float.TryParse(num2, out float snum) )
+
+                        num:                
+                        Console.WriteLine("Enter another number:");
+                        string Number2=(Console.ReadLine());
+
+                   // Checks if the input is numeric or non-numeric
+                   if (!float.TryParse(Number2, out float SecondNumber) )
             {
                 Console.WriteLine("Invalid input. Please try again...");
                 goto num;
             }
-		  
-		   
-			
-		    // switch case for the operators
-			switch (choice)
+
+
+
+                    // switch case for the operators
+                        switch (choice)
         {
             case "+":
-			    fnum=fnum+snum;
+                            FirstNumber=FirstNumber+SecondNumber;
                 goto choice;
-				break;
+                                break;
             case "-":
-			    fnum=fnum-snum;
+                            FirstNumber=FirstNumber-SecondNumber;
                goto choice;
-				break;
+                                break;
             case "*":
-			    fnum=fnum*snum;
+                            FirstNumber=FirstNumber*SecondNumber;
               goto choice;
-			    break;
-		    case "/":
-			    fnum=fnum/snum;
+                            break;
+                    case "/":
+                            FirstNumber=FirstNumber/SecondNumber;
                goto choice;
-				break;
-	
-				break;
-		    default:
+                                break;
+                    default:
                 Console.WriteLine($"Invalid input.");
-				goto choice;
+                                goto choice;
         }
-			
-			
-		   
-		
-		
-		// Another goto to perform mini loop
-		yesNo:
-		Console.WriteLine("\nDo you want to try again?(Y/N): ");
-		string yesNo= Console.ReadLine();
-		
-		//Checks if the user wanted to try again
-		if (yesNo != "N" && yesNo != "n" && yesNo !="y" && yesNo!="Y")
-		{ 
+
+
+
+
+
+                // Another goto to perform mini loop
+                yesNo:
+                Console.WriteLine("\nDo you want to try again?(Y/N): ");
+                string yesNo= Console.ReadLine();
+
+                //Checks if the user wanted to try again
+                if (yesNo != "N" && yesNo != "n" && yesNo !="y" && yesNo!="Y")
+                { 
             Console.WriteLine("Invalid input, Please try again...");
             goto yesNo;
         }
-		
-		else if (yesNo == "N" || yesNo == "n")
-		{ 
+
+                else if (yesNo == "N" || yesNo == "n")
+                { 
             Console.WriteLine("Thanks for using my program!");
             Environment.Exit (0);
         }
-            
+
             }
         }
     }
 }
-
-
 
 
 
