167e6cf259d2cbdd329623fb45230870eea21a3a
Edzra Macas <EdzraM7@gmail.com>
2024-04-13 22:14:17 +0800

    Proper naming conventions

--- oop/Calculator.cs
+++ oop/Calculator.cs
@@ -10,15 +10,15 @@ namespace Calculator
             {
                 Console.WriteLine("\nCalculator. Press (=) to finish the calculation");
                 double result = 0;
-                double num;
+                double firstNumber;
                 string userInput;
                 // Prompts for the first number then assigns it to result
                 while (true)
                 {
                     userInput = Console.ReadLine();
-                    if (double.TryParse(userInput, out num))
+                    if (double.TryParse(userInput, out firstNumber))
                     {
-                        result = num;
+                        result = firstNumber;
                         break;
                     }
                     else
@@ -29,40 +29,40 @@ namespace Calculator
                 // Prompts for an operator and number to be calculated with result
                 while (true)
                 {
-                    string operatr = Console.ReadLine();
-                    if (operatr == "=") // Will check for (=) to exit the loop
+                    string operationInput = Console.ReadLine();
+                    if (operationInput == "=") // Will check for (=) to exit the loop
                     {
                         break;
                     }
                     // will reiterate the loop if operator entered is invalid
-                    if (operatr != "+" && operatr != "-" && operatr != "*" && operatr != "/")
+                    if (operationInput != "+" && operationInput != "-" && operationInput != "*" && operationInput != "/")
                     {
                         Console.WriteLine("Invalid operator. Please enter (+, -, *, /) only or (=) to finish the calculation.");
                         continue;
                     }
                     // Prompts for the num to be calculated wit the result
-                    double nextNum;
-                    if (!double.TryParse(Console.ReadLine(), out nextNum))
+                    double nextNumber;
+                    if (!double.TryParse(Console.ReadLine(), out nextNumber))
                     {
                         Console.WriteLine("Invalid input. Please enter a number.");
                         continue;
                     }
                     // Calculations to do based of operator
-                    switch (operatr)
+                    switch (operationInput)
                     {
                         case "+":// Addition
-                            result += nextNum;
+                            result += nextNumber;
                             break;
                         case "-": // Subtraction
-                            result -= nextNum;
+                            result -= nextNumber;
                             break;
                         case "*": // Multiplication
-                            result *= nextNum;
+                            result *= nextNumber;
                             break;
                         case "/": // Division
-                            if (nextNum != 0)
+                            if (nextNumber != 0)
                             {
-                                result /= nextNum;
+                                result /= nextNumber;
                             }
                             else
                             {
@@ -75,12 +75,12 @@ namespace Calculator
                 // Prints the result of the calculation
                 Console.WriteLine("Result: " + result);
 
-                string another;
+                string anotherCalculation;
                 while (true)// Will continue to prompt untill user inputs a valid choice
                 {
                     Console.Write("Do you want to perform another calculation (y/n)? ");
-                    another = Console.ReadLine().ToLower();
-                    if (another == "n" || another == "y")
+                    anotherCalculation = Console.ReadLine().ToLower();
+                    if (anotherCalculation == "n" || anotherCalculation == "y")
                     {
                         break;
                     }
@@ -90,7 +90,7 @@ namespace Calculator
                     }
                 }
                 // Will only stop the program if the user enters N/n
-                if (another == "n")
+                if (anotherCalculation == "n")
                 {
                     Console.WriteLine("\nExiting the program...");
                     break;
