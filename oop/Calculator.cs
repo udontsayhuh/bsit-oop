a48981bb55a84ea5b06019d4098592c452902181
Bengie <bvillesco@gmail.com>
2024-04-13 20:14:31 +0800

    Refactor: Create Display class to organize display all values and operation option methods. Replace foreach loop with for loop for optimization in DisplayAllValuesInput method.

--- oop/Calculator.cs
+++ oop/Calculator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -23,9 +24,14 @@ namespace Calculator
         // Abstract method.
         public abstract void UserInput();   // Method for user input.
         public abstract void Calculate();   // Method for calculation.
+    }
 
+
+    // Create another class called Display
+    class Display
+    {
         // Regular Method.
-        public void DisplayOperationOption()
+        public static void DisplayOperationOption()
         {
             Console.WriteLine("──────────────────────────────────────────────────────");
             Console.WriteLine("                     CALCULATOR                       ");
@@ -37,11 +43,16 @@ namespace Calculator
             Console.WriteLine("──────────────────────────────────────────────────────");
         }
 
-        public void DisplayAllValuesInputted()
+        public static void DisplayAllValuesInput(List<dynamic> userInputValues)
         {
             Console.Clear();    // Clear the console screen.
             DisplayOperationOption();   // Method call to DisplayOperationOption method.
-            foreach (dynamic val in Operand) { Console.Write($"{val} "); }  // Display all the values stored in the list called Operand.
+
+            // Display all the values that the user's input (including operation).
+            for (int i = 0; i < userInputValues.Count; i++)
+            {
+                Console.Write($"{userInputValues[i]} ");
+            }
             Console.WriteLine("\n");
         }
     }
@@ -55,43 +66,43 @@ namespace Calculator
         public override void UserInput()
         {
             char chosenOperator = ' ';
-            while (chosenOperator != '=')   // Loop as long as the user does not enter an equal sign for the operator.
+            while (chosenOperator != '=')   // Loop as long as the user does not enter an equal sign for the operator. (Outer loop)
             {
                 // Error handling.
-                while (true)
+                while (true)    // First inner loop.
                 {
                     try // Execute and test for errors, if any occurs handle them with cath statement.
                     {
                         Console.Write("Enter number for Operand: ");
-                        Operand.Add(Convert.ToDouble(Console.ReadLine()));  // Add the user inputted to the list of Operand.
-                        DisplayAllValuesInputted(); // Method call to DisplayAllValuesInputted method.
-                        break;
+                        Operand.Add(Convert.ToDouble(Console.ReadLine()));  // Add the user input to the list of Operand.
+                        Display.DisplayAllValuesInput(Operand); // Method call to DisplayAllValuesInput method in the Display class with parameter Operand(list).
+                        break;  // Exit the while loop.
                     }
                     catch (Exception e)
-                    {   // Execute this statement if the user inputted is not a number.
-                        DisplayAllValuesInputted(); // Method call to DisplayAllValuesInputted method.
+                    {   // Execute this statement if the user input is not a number.
+                        Display.DisplayAllValuesInput(Operand); // Method call to DisplayAllValuesInput method in the Display class with parameter Operand(list).
                         Console.WriteLine("Invalid input! Please input a valid value!");
                     }
-                }
+                }   // End of first inner loop.
 
                 Console.Write("Enter operator: ");
                 chosenOperator = Convert.ToChar(Console.Read());
 
                 // Validate the chosen operator.
-                while (chosenOperator != '+' && chosenOperator != '*' && chosenOperator != '-' && chosenOperator != '/' && chosenOperator != '=')
+                while (chosenOperator != '+' && chosenOperator != '*' && chosenOperator != '-' && chosenOperator != '/' && chosenOperator != '=')   // Second inner loop.
                 {
                     Console.ReadLine();
-                    DisplayAllValuesInputted(); // Method call to DisplayAllValuesInputted method.
+                    Display.DisplayAllValuesInput(Operand); // Method call to DisplayAllValuesInput method in the Display class with parameter Operand(list).
 
                     Console.Write("Invalid operator!\nEnter valid operator (+, *, -, /, =): ");
                     chosenOperator = Convert.ToChar(Console.Read());
-                }
+                }   // End of second inner loop.
                 Console.ReadLine();
-                Operand.Add(chosenOperator);    // Add the user inputted for the operator to the list of Operand.
-                DisplayAllValuesInputted();     // Method call to DisplayAllValuesInputted method.
-            }
+                Operand.Add(chosenOperator);    // Add the user input for the operator to the list of Operand.
+                Display.DisplayAllValuesInput(Operand);     // Method call to DisplayAllValuesInput method in the Display class with parameter Operand(list).
+            }   // End of outer loop.
             Console.Clear();    // Clear the console scree.
-            DisplayOperationOption();   // Diplay operation option using the DisplayOperationOption method.
+            Display.DisplayOperationOption();   // Diplay operation option using the DisplayOperationOption method in the Display class.
             Calculate();    // Method call to Calculate method.
         }
 
@@ -144,7 +155,7 @@ namespace Calculator
             {   // Loop as long as the value of anotherCalcu is 'Y' (outer loop).
                 // Create a new list that accept any data type and passed the 0.0 as an argument to the constructor of BasicCalculator.
                 BasicCalculator calculateValues = new BasicCalculator(new List<dynamic>(), 0.0);
-                calculateValues.DisplayOperationOption();   // Diplay operation option using the DisplayOperationOption method.
+                Display.DisplayOperationOption();   // Diplay operation option using the DisplayOperationOption method.
                 calculateValues.UserInput();    // Get the user input for operands and operator using the UserInput method.
 
                 // Ask user if the user want to perform another calculation.
