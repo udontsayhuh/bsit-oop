424bb1a322ab187791e35303e3fee82feaa7640a
Ashley Marie Erillo <163630955+AshleyErillo@users.noreply.github.com>
2024-04-18 13:04:24 +0800

    Update Calculator.cs

--- Calculator.cs
+++ Calculator.cs
@@ -1,8 +1,7 @@
 using System;
 
-namespace Calculatorassignment
+namespace Calculatorcs
 {
-    //Parent Class
     class Calculator
     {
         //Encapsulation for private fields
@@ -22,12 +21,12 @@ namespace Calculatorassignment
         }
 
         // Calculations for the operators
-        public double Calculation(double second_value, char operatorchoice)
+        public double Calculation(double second_value, char operatorChoice)
         {
             double result = 0;
             try
             {
-                switch (operatorchoice)
+                switch (operatorChoice)
                 {
                     case '+':
                         result = First_value + second_value;
@@ -52,114 +51,115 @@ namespace Calculatorassignment
             }
             catch
             {
-                Console.Write("Invalid value. Please enter a valid number: ");
+                Console.Write("Invalid value. \nPlease enter a valid number: ");
             }
             return result;
         }
         private void Default()
         {
-            Console.Write("Invalid operator. Please enter a valid operator (+, -, *, /, =):");
+            Console.Write("Invalid operator. \nPlease enter a valid operator (+, -, *, /, =):");
         }
-    }
-    class Program
-    {
-        static void Main(string[] args)
+        class Program
         {
-            try
+            static void Main(string[] args)
             {
-                //using do-while loop to repeat the process if the user wants to calculate again
-                string choice;
-                do
+               try
                 {
-                    Console.Write("Enter a value: ");
-                    double First_value;
-                    while (!double.TryParse(Console.ReadLine(), out First_value))
-                    {
-                        Console.Write("Invalid value. Please enter a valid number: ");
-                    }
-
-                    char operatoruse;
-                    bool validOperator = false;
+                    //using do-while loop to repeat the process if the user wants to calculate again
+                    string choice;
                     do
                     {
-                        Console.WriteLine("+ - * / ");
-                        Console.Write("Choose operator to use: ");
-                        if (!char.TryParse(Console.ReadLine(), out operatoruse))
+                        Console.Write("Enter a value: ");
+                        double First_value;
+                        while (!double.TryParse(Console.ReadLine(), out First_value))
                         {
-                            Console.WriteLine("Invalid input. Please enter a valid operator.");
-                            continue;
+                            Console.Write("Invalid value. \nPlease enter a valid number: ");
                         }
 
-                        if (operatoruse == '+' || operatoruse == '-' || operatoruse == '*' || operatoruse == '/')
-                        {
-                            validOperator = true;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Invalid operator. Please enter a valid operator (+, -, *, /).");
-                        }
-                    } while (!validOperator);
-
-
-                    Console.Write("Enter a value: ");
-                    double second_value;
-                    while (!double.TryParse(Console.ReadLine(), out second_value))
-                    {
-                        Console.Write("Invalid value. Please enter a valid number: ");
-                    }
-
-                    Calculator calculator = new Calculator(First_value);
-                    double result = calculator.Calculation(second_value, operatoruse);
-
-                    while (true)
-                    {
-                        Console.WriteLine("\n+ - * / ");
-                        Console.Write("Choose operator to use (Enter an = to finish): ");
-                        if (!char.TryParse(Console.ReadLine(), out operatoruse))
-                        {
-                            Console.WriteLine("Invalid input. Please enter a valid operator.");
-                            continue;
-                        }
-
-                        if (operatoruse == '=')
-                            break;
-
-                        if (operatoruse == '+' || operatoruse == '-' || operatoruse == '*' || operatoruse == '/')
-                        {
-                            validOperator = true;
-                        }
-                        else
-                        {
-                            Console.Write("Invalid operator. Please enter a valid operator (+, -, *, /): ");
-                            continue;
-                        }
-
+                        char operatorUse;
+                        bool validOperator = false;
                         do
                         {
-                            Console.Write("\nEnter a value: ");
-                        } while (!double.TryParse(Console.ReadLine(), out second_value));
+                            Console.WriteLine("\n+ - * / ");
+                            Console.Write("Choose operator to use: ");
+                            if (!char.TryParse(Console.ReadLine(), out operatorUse))
+                            {
+                                Console.WriteLine("Invalid input. \nPlease enter a valid operator.");
+                                continue;
+                            }
+
+                            if (operatorUse == '+' || operatorUse == '-' || operatorUse == '*' || operatorUse == '/')
+                            {
+                                validOperator = true;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Invalid operator. \nPlease enter a valid operator (+, -, *, /).");
+                            }
+                        } while (!validOperator);
 
 
-                        calculator.First_value = result;
-                        result = calculator.Calculation(second_value, operatoruse);
+                        Console.Write("Enter a value: ");
+                        double second_value;
+                        while (!double.TryParse(Console.ReadLine(), out second_value))
+                        {
+                            Console.Write("Invalid value. \nPlease enter a valid number: ");
+                        }
+
+                        Calculator calculator = new Calculator(First_value);
+                        double result = calculator.Calculation(second_value, operatorUse);
+
+                        while (true)
+                        {
+                            Console.WriteLine("\n+ - * / ");
+                            Console.Write("Choose operator to use (Enter an = to finish): ");
+                            if (!char.TryParse(Console.ReadLine(), out operatorUse))
+                            {
+                                Console.WriteLine("Invalid input. \nPlease enter a valid operator.");
+                                continue;
+                            }
+
+                            if (operatorUse == '=')
+                                break;
+
+                            if (operatorUse == '+' || operatorUse == '-' || operatorUse == '*' || operatorUse == '/')
+                            {
+                                validOperator = true;
+                            }
+                            else
+                            {
+                                Console.Write("Invalid operator. \nPlease enter a valid operator (+, -, *, /): ");
+                                continue;
+                            }
+
+                            Console.Write("Enter a value: ");
+                            while (!double.TryParse(Console.ReadLine(), out second_value))
+                            {
+                                Console.Write("Invalid value. \nPlease enter a valid number: ");
+                            }
+
+                            calculator.First_value = result;
+                            result = calculator.Calculation(second_value, operatorUse);
+                        }
+
+                        Console.WriteLine($"The result will be: {result}");
+
+                        Console.Write("\nDo you want to make another calculation? (Yes/No): ");
+                        choice = Console.ReadLine().ToLower();
+                    } while (choice == "yes");
+
+                    if (choice == "no")
+                    {
+                        Console.WriteLine("End of Program");
                     }
 
-                    Console.WriteLine($"The result will be: {result}");
-
-                    Console.Write("\nDo you want to make another calculation? (Yes/No): ");
-                    choice = Console.ReadLine().ToLower();
-                } while (choice == "yes");
-
-                if (choice == "no")
-                {
-                    Console.WriteLine("End of Program");
                 }
-
-            }
-            catch
-            {
-                Console.WriteLine("Invalid Value.");
+                catch
+                {
+                    Console.WriteLine("Invalid Value.");
+                }
             }
         }
     }
 }
+
