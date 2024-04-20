320f8e751d7976e02535f1e29cd393b0e8e4fc8c
Kristine Mae Cleofas <163546627+Krxtinezx@users.noreply.github.com>
2024-04-20 19:23:00 +0800

    Update sums.cs
    Sums of two integer, double, and sum of the product

--- Coding Challenges/sums.cs
+++ Coding Challenges/sums.cs
@@ -7,16 +7,19 @@ using System;
 
 class Calculator
 {
+    //method to sum the two integers
     public int SumIntegers(int num1, int num2)
     {
         return num1 + num2;
     }
 
+   //method to sum the two doubles
     public double SumDoubles(double num1, double num2)
     {
         return num1 + num2;
     }
 
+    //method to calculate the product sum integer and double
     public double ProductOfSums(int sumIntegers, double sumDoubles)
     {
         return sumIntegers * sumDoubles;
@@ -29,21 +32,26 @@ class Program
     {
         Calculator calculator = new Calculator();
 
+        //getting the input from the user
         int int1 = GetIntegerInput("Enter First Integer: ");
         int int2 = GetIntegerInput("Enter Second Integer: ");
         double double1 = GetDoubleInput("\nEnter First Double: ");
         double double2 = GetDoubleInput("Enter Second Double: ");
 
+        //calculating the sum of integers and doubles
         int sumIntegers = calculator.SumIntegers(int1, int2);
         double sumDoubles = calculator.SumDoubles(double1, double2);
 
+        //display the sums
         Console.WriteLine($"\nSum of Integers: {sumIntegers}");
         Console.WriteLine($"Sum of Doubles: {sumDoubles}");
 
+        //calculate the product of the sums and display it
         double productofSums = calculator.ProductOfSums(sumIntegers, sumDoubles);
         Console.WriteLine($"Product of Sums: {productofSums}");
     }
 
+    //method to get the integer input from the user
     public static int GetIntegerInput(string message)
     {
         while (true)
@@ -55,11 +63,14 @@ class Program
             }
             else
             {
+                //invalid if user did not put an integer 
                 Console.WriteLine("Invalid input! Please enter an integer.");
             }
         }
     }
+    
 
+    //method to get the double input from the user
     public static double GetDoubleInput(string message)
     {
         while (true)
@@ -71,6 +82,7 @@ class Program
             }
             else
             {
+                //invalid if user did not put a double input
                 Console.WriteLine("Invalid input! Please enter a double.");
             }
         }
