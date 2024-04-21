5a26d553339c20d91cd8089a9e6e412ce54c5fea
Crystal D <138987064+talrubd@users.noreply.github.com>
2024-04-21 22:21:49 +0800

    Create Coding_Challenge_4.cs
    Write a C# program that takes two numbers as input: the first number will be the number to be multiplied 
            and the second number will be the multiplier, and prints its multiplication table up to the given 
            second number (the multiplier).

---
+++ oop/Coding_Challenge_4.cs
@@ -0,0 +1,29 @@
+using System;
+using System.ComponentModel;
+using System.Net.Http.Headers;
+
+namespace CrystalynDanga
+{
+    class Program
+    {
+        /*Write a C# program that takes two numbers as input: the first number will be the number to be multiplied 
+        and the second number will be the multiplier, and prints its multiplication table up to the given 
+        second number (the multiplier).*/
+        static void Main(string[] args)
+        {
+            int result = 0;
+            Console.Write("Enter the number to be multiplied: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the multiplier: ");
+            int multiplier = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i <= multiplier; i++) 
+            {
+                result = i * multiplier;
+                Console.WriteLine($"{number} * {i} = {result}");
+                result = 0;
+            }
+        }
+    }
+}
