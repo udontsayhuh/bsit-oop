23d2e0942363cc552b56afae50e164aa0c111532
Crystal D <138987064+talrubd@users.noreply.github.com>
2024-04-21 20:47:37 +0800

    Create Coding_Challenge_2.cs
    Write a C# program that accepts a string and will count the number of words in the provided string and print the string in uppercase as a result.

---
+++ oop/Coding_Challenge_2.cs
@@ -0,0 +1,40 @@
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Principal;
+using System.Xml.XPath;
+
+namespace CrystalynDanga
+{
+    /*Write a C# program that accepts a string and will count the number of words in the provided string and print the string in uppercase as a result.*/
+
+
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            int wordCounter = 1; // will treat evey sentence to have 1 minimum word
+            
+            // gets user input
+            Console.Write("Enter a string: ");
+            string userInput = Console.ReadLine();
+
+            foreach (char letter in userInput) // loop for checking the string
+            {
+                if (char.IsWhiteSpace(letter)) // will check if letter variable is a whitespace
+                {
+                    wordCounter++; // If yes, it will add number to wordCounter
+                }
+            }
+
+            Console.WriteLine("Number of words: " + wordCounter);
+
+            userInput = userInput.ToUpper(); // Converts string to uppercase.
+            Console.WriteLine(userInput);
+
+
+
+        } // Main Method
+    } // Program Class
+
+} // Namespace
