bb060e8575993ceb97c8baba35bebd33d5644024
Lawrence Afable <123860713+LawrenceAfable@users.noreply.github.com>
2024-04-21 18:40:54 +0800

    Update Q5.cs

--- Code Challenges /Q5.cs
+++ Code Challenges /Q5.cs
@@ -1,1 +1,22 @@
+using System;
 
+class Program {
+    static void Main(string[] args){
+         
+		string[] cars = {"Toyota", "Ford", "Honda", "Tesla", "Volvo"};
+		
+		// UNSORTED
+		Console.WriteLine("UNSORTED LIST: ");
+		for (int i = 0; i < cars.Length; i++) {
+            Console.WriteLine(cars[i]);
+        }
+		  
+		// SORTED
+		Console.WriteLine("\nSORTED LIST: ");
+		Array.Sort(cars);
+		for (int i = 0; i < cars.Length; i++) {
+            Console.WriteLine(cars[i]);
+        }
+        
+    } // Main
+} // Program
