ed7cda9e48f05d092c49302203556e4c1bc40afa
Lawrence Afable <123860713+LawrenceAfable@users.noreply.github.com>
2024-04-21 18:34:52 +0800

    Update Q2.cs

--- Code Challenges /Q2.cs
+++ Code Challenges /Q2.cs
@@ -1,1 +1,38 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Input
+        Console.Write("Enter something: ");
+        string str = Console.ReadLine();
+
+        int count = 0;
+        bool inWord = false;
+
+        foreach (char c in str)
+        {
+            if (char.IsWhiteSpace(c))
+            {
+                if (inWord)
+                {
+                    count++;
+                    inWord = false;
+                }
+            }
+            else
+            {
+                inWord = true;
+            }
+        }
+
+        if (inWord)
+        {
+            count++;
+        }
+
+        Console.WriteLine($"There are {count} word/s in \"{str.ToUpper()}\".");
+    }
+}
 
