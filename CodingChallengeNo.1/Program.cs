964afc8aaa18121ea7bd916df3e0c5551a3ee77e
shnlmra <shanellaacagulang@iskolarngbayan.pup.edu.ph>
2024-04-22 20:53:46 +0800

    firschallenge

---
+++ CodingChallengeNo.1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class userInput
+{
+    static void Main(string[] args)
+    {
+        int intnum1;
+        int intnum2;
+        double doublenum1;
+        double doublenum2;
+        
+
+        Console.Write("Enter a number:");
+        intnum1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter a number:");
+        intnum2 = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter a double:");
+        doublenum1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter a double:");
+        doublenum2 = int.Parse(Console.ReadLine()); 
+
+        int intsum = intnum1 + intnum2;
+        Console.WriteLine($"Sum of two integers is: {intsum}");
+
+        double doublesum = doublenum1 + doublenum2; 
+        Console.WriteLine($"Sum of two integers is: {doublesum}");
+
+        double product = doublesum * intsum;
+        Console.WriteLine($"Product of two sum: {product}");
+
+
+
+    }
+}
