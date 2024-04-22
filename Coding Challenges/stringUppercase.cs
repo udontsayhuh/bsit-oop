3dd375a92ce68ea6e0bcbffd63fae6f9ec4078dd
Kristine Mae Cleofas <163546627+Krxtinezx@users.noreply.github.com>
2024-04-22 16:38:31 +0800

    Update stringUppercase.cs
    code challenges number 2 - string in uppercase

--- Coding Challenges/stringUppercase.cs
+++ Coding Challenges/stringUppercase.cs
@@ -1,1 +1,30 @@
+/*Write a C# program that accepts a string and will count the number of words 
+in the provided string and print the string in uppercase as a result.*/
+using System;
 
+class Program
+{
+  public static void Main()
+  {
+    //ask the user to enter a string (word/s)
+    Console.WriteLine("Enter a string: ");
+    //reads the user's input
+    string input = Console.ReadLine();
+
+    //count the number of words in the input string
+    int WordCount = CountWords(input);
+    string uppercaseString = input.ToUpper(); //convert the input string to uppercase
+
+    //display the number of words
+    Console.WriteLine($"Number of Words: {WordCount}");
+    //display the input string in uppercase
+    Console.WriteLine($"Uppercase String: {uppercaseString}");
+  }
+
+  public static int CountWords(string input)
+  {
+    //split the input string by whitespace characters
+    string[] words = input.Split(new[] {' '});
+    return words.Length; //return the number of words
+  }
+}
