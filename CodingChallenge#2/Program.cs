dcb870d66560a9b4734c84bcf2d642b6f6461ddf
shnlmra <shanellaacagulang@iskolarngbayan.pup.edu.ph>
2024-04-22 20:55:46 +0800

    secondnumber

---
+++ CodingChallenge#2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class CountNum
+{
+    public static void Main()
+    {
+        string strcount;
+        int s, wordcount;
+
+        Console.WriteLine("Coding Challenge No. 2\n");
+        Console.Write("Input String:");
+        strcount = Console.ReadLine();
+
+        s = 0;
+        wordcount = 1;
+
+        while (s <= strcount.Length - 1)
+        {
+            if (strcount[s] == ' ' || strcount[s] == '\n' || strcount[s] == '\t');
+            {
+                wordcount++;
+            }
+
+            s++;
+        }
+;
+        Console.Write("Total number of strings:{0}\n Converted Uppercase String: {1}", wordcount, strcount.ToUpper());
+    }
+
+}
