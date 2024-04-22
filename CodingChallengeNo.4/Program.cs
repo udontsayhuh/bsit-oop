6de74d00403e2931820b2082963d37673679e89f
shnlmra <shanellaacagulang@iskolarngbayan.pup.edu.ph>
2024-04-22 20:57:46 +0800

    fourthchallenge

---
+++ CodingChallengeNo.4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Multiply
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the number to be multiplied: ");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter the multiplier: ");
+        int nummultiplier = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Multiplication table for {num1} up to {nummultiplier}:");
+        for (int i = 1; i <= nummultiplier; i++)
+        {
+            for (int j = 1; j <= num1; j++)
+            {
+                Console.Write($"{j} x {i} = {j * i}\n");
+            }
+            Console.WriteLine();
+        }
+    }
+}
