7e415d64f41f117ce19eaa7496b5196583f33dcf
shnlmra <shanellaacagulang@iskolarngbayan.pup.edu.ph>
2024-04-22 20:58:21 +0800

    fifthchallenge

---
+++ CodingChallengeNo.5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+class List
+{
+    static void Main(string[] args)
+
+    {
+        List<string> carBrand = new List<string>();
+
+        carBrand.Add("BMW");
+        carBrand.Add("Audi");
+        carBrand.Add("Volkswagen");
+        carBrand.Add("Chevrolet");
+        carBrand.Add("Porche");
+
+        carBrand.Sort();
+
+        Console.Write("Sorted list of Car Brands:\n");
+        foreach (string item in carBrand)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
