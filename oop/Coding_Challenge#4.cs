0e09b514a77b6d41d1b2cca8f8ee1e1de8779129
Carl Aldrey Bergado <carlaldreydbergado@iskolarngbayan.pup.edu.ph>
2024-04-23 23:10:42 +0800

    Coding ChallConding_Challenge#4 RC VER.

---
+++ oop/Coding_Challenge#4.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+abstract class Multiplication // Base class (parent)
+{
+    public int Multiplicand { get; set; }
+    public int Multiplier { get; set; }
+
+    protected Multiplication(int valuet, int value)
+    {
+        Multiplicand = value;
+        Multiplier = value;
+    }
+
+    public abstract void Display();
+}
+
+class Compute : Multiplication
+{
+    public Compute(int multiplicand, int multiplier) : base(multiplicand, multiplier)
+    {
+        this.Multiplicand = multiplicand;
+        this.Multiplier = multiplier;
+    }
+
+    public override void Display()
+    {
+        for (int i = 1; i <= Multiplier; i++)
+        {
+            int product = Multiplicand * i;
+            Console.WriteLine(Multiplicand + " * " + i + " = " + product);
+
+        }
+    }
+
+}
+
+
+class Bergado_Assignment
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter your multiplicand: ");
+        int multiplicand = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter your multiplier: ");
+        int multiplier = Convert.ToInt32(Console.ReadLine());
+        Compute compute = new Compute(multiplicand, multiplier);
+        compute.Display();
+
+
+
+    }
+}
