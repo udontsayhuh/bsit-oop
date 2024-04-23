1a5be4dc18149a773efda9e4625635185105bf3b
Mark Regie A. Magtangob <110184282+Markxregie@users.noreply.github.com>
2024-04-23 22:13:51 +0800

    C# Coding Challenge 2

---
+++ C# Coding Challenge  2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a string: ");
+        string input = Console.ReadLine();
+
+        int wordCount = input.Split(' ').Length;
+        string uppercaseInput = input.ToUpper(); 
+
+        Console.WriteLine($"Number of words: {wordCount}");
+        Console.WriteLine($"Uppercase string: {uppercaseInput}");
+    }
+}
