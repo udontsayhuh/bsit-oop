8ee30182a12eed146eb6e04164bba2fa05848631
LennoxMacadangdang <163654511+LennoxMacadangdang@users.noreply.github.com>
2024-04-23 19:11:31 +0800

    Update Codingpractices4.cs

--- oop/Codingpractices4.cs
+++ oop/Codingpractices4.cs
@@ -1,1 +1,22 @@
+class MultiplicationTable
+{
+    static void Main(string[] args)
+    {
+      //User enters number to be multiplied
+        Console.Write("Enter the number to be multiplied: ");
+        int number = Convert.ToInt32(Console.ReadLine());
 
+      // User enters the multiplier for the table
+        Console.Write("Enter the multiplier: ");
+        int multiplier = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"Multiplication table for {number}:");
+
+      //Program iterates till the number is less than or equal to the multiplier
+        for (int i = 1; i <= multiplier; i++)
+        {
+            int result = number * i;
+            Console.WriteLine($"{number} * {i} = {result}");
+        }
+    }
+}
