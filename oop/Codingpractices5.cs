f9c2c30a87d6ae221095dec156e5a32068a363f9
LennoxMacadangdang <163654511+LennoxMacadangdang@users.noreply.github.com>
2024-04-23 19:19:03 +0800

    Update Codingpractices5.cs

--- oop/Codingpractices5.cs
+++ oop/Codingpractices5.cs
@@ -1,1 +1,26 @@
+using System.Collections;
 
+class Cars
+{
+    static void Main(string[] args)
+    {
+        ArrayList cars = new ArrayList();
+
+        // Adding cars to the list
+        cars.Add("Toyota Vios");
+        cars.Add("Honda City");
+        cars.Add("Mitsubishi Mirage");
+        cars.Add("Nissan Almera");
+        cars.Add("Kia Rio");
+
+        // Sorting the list
+        cars.Sort();
+
+        // Displaying the sorted list
+        Console.WriteLine("Sorted list of cars:");
+        foreach (string car in cars)
+        {
+            Console.WriteLine(car);
+        }
+    }
+}
