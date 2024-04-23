6d16d3d61712cb6f06d2f90327b59f74aa9edec1
Mark Regie A. Magtangob <110184282+Markxregie@users.noreply.github.com>
2024-04-23 22:15:16 +0800

    C# Coding Challenge 4

---
+++ C# Coding Challenge 4.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the number to be multiplied: ");
+        int number = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter the multiplier: ");
+        int multiplier = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Multiplication table for {number} up to {multiplier}:");
+
+        for (int i = 1; i <= multiplier; i++)
+        {
+            int result = number * i;
+            Console.WriteLine($"{number} x {i} = {result}");
+        }
+    }
+}
