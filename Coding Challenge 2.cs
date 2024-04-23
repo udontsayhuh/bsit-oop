d4214084d889ff9292aeaba13429e9e7b43699b6
yengyeng <162948785+yshaprz@users.noreply.github.com>
2024-04-23 22:53:41 +0800

    Coding Challenge 2

---
+++ Coding Challenge 2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+//A program that count the number of words in the provided string
+class WordCount
+{
+    public static void Main(string[] args)
+    {
+        string str1 = ("Hi! I am Yesha Perez"); //to initialized string
+        string[] word = str1.Split(' '); //method to count the words of str1
+
+        int countW = word.Length; //to count the length of the entered word in str1
+
+        //to print the output in uppercase
+        Console.WriteLine("ENTERED STRING: " + str1.ToUpper());
+        Console.WriteLine("\nTHE NUMBER OF WORDS IN THE ENTERED STRING IS: " + countW);
+    }
+}
