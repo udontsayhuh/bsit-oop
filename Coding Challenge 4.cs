5124abff82ae641ef7c51ed872ba81b647a6dfd0
yengyeng <162948785+yshaprz@users.noreply.github.com>
2024-04-23 23:06:38 +0800

    Add files via upload

---
+++ Coding Challenge 4.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+//A program that multiplies two integers and displays its multiplication table
+class Multiplication
+{
+    public static void Main(string[] args)
+    {
+        int num1 = Convert.ToInt32(Console.ReadLine()); //integer 1
+        int num2 = Convert.ToInt32(Console.ReadLine()); //integer 2
+
+        Console.WriteLine("\nRESULTS: \n");
+
+        //for loop to iterate each multiplication expressions
+        for (int i = 1; i <= num2; i++)
+        {   
+            //to print the result 
+            Console.WriteLine("{0} x {1} = {2}", num1, i, num1 * i);
+        }
+    }
+}
