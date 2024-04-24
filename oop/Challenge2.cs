6165906a23409f794485d3c04800c3f3adb8333b
Christelle Anne <christelleannesdacapias@iskolarngbayan.pup.edu.ph>
2024-04-24 20:21:30 +0800

    Problem 2

---
+++ oop/Challenge2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Sentence
+{
+    public virtual void My_String()
+    {
+        Console.Write("\nEnter a string: ");
+        string word = Console.ReadLine();
+
+        string[] wrds = word.Split(' '); //split the sentence into words
+
+        int wordCount = wrds.Length; //counts the words
+
+        Console.Write($"\nNumber of Words: {wordCount}\n");
+
+        string uppercase = word.ToUpper(); //changes the string into uppercase
+
+        Console.Write($"\nUppercase: {uppercase}\n\n");
+    }
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Sentence sentence = new Sentence(); //instantiate the class
+        sentence.My_String(); //calls the method
+    }
+
+}
