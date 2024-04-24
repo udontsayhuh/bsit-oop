7ef91f8db4c831592368c9ca3de18502f4b36e76
nerieannbravo <163762915+nerieannbravo@users.noreply.github.com>
2024-04-24 23:17:32 +0800

    Update multiplication-table.cs

--- Coding-Challenges/multiplication-table.cs
+++ Coding-Challenges/multiplication-table.cs
@@ -4,13 +4,27 @@ class Program
 {
     static void Main()
     {
+        int number = 0;
+        int multiplier = 0;
+        
         // Get the number to be multiplied and the multiplier from the user
-        Console.Write("Enter the number to be multiplied: ");
-        int number = int.Parse(Console.ReadLine());
-
-        Console.Write("Enter the multiplier (up to which you want the table): ");
-        int multiplier = int.Parse(Console.ReadLine());
-
+        while (true)
+    {
+        try
+        {   
+            Console.Write("Enter the number to be multiplied: ");
+            number = Convert.ToInt32(Console.ReadLine());
+        
+            Console.Write("Enter the multiplier (up to which you want the table): ");
+            multiplier = Convert.ToInt32(Console.ReadLine());
+            break;
+        }
+        
+        catch
+        {
+            Console.WriteLine("Invalid Input. Integer Only.");
+        }
+    }
         // Print the multiplication table
         Console.WriteLine($"Multiplication table for {number} up to {multiplier}:");
         for (int i = 1; i <= multiplier; i++)
