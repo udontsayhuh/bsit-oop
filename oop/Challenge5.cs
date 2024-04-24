c0e6e6deb166d99aa176c22f95d88becb7d61413
Christelle Anne <christelleannesdacapias@iskolarngbayan.pup.edu.ph>
2024-04-24 20:29:34 +0800

    Problem 5

---
+++ oop/Challenge5.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] car = { "Honda", "BMW", "Toyota", "Mazda", "Nissan", "Geely" }; //array of cars
+        Console.WriteLine("Original List:");
+        for (int i = 0; i < car.Length; i++) //loops the array of cars
+        {
+            Console.WriteLine(car[i]); //prints the car per line
+        }
+
+        Array.Sort(car); //sort the array
+        Console.WriteLine("\nSorted List:");
+        foreach (string i in car) //loops the sorted array
+        {
+            Console.WriteLine(i); //prints the sorted car per line
+        }
+    }
