9cc9fb0a684bc1b5158166841da120aa4f4b117e
Christelle Anne <christelleannesdacapias@iskolarngbayan.pup.edu.ph>
2024-04-24 20:26:17 +0800

    Problem 4

---
+++ oop/Challenge4.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class Program
+{
+    static double num; //declare variables
+    static int mul;
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a number: ");
+        while (!double.TryParse(Console.ReadLine(), out num)) //loops until valid numerical value is entered 
+        {
+            Console.WriteLine("Invalid input. Only numerical value is accepted.\n");
+            Console.Write("Enter a number: ");
+        }
+
+        Console.Write("\nEnter a multiplier: ");
+        while (!int.TryParse(Console.ReadLine(), out mul)) //loops until valid integer value is entered 
+        {
+            Console.WriteLine("Invalid input. Only integer value is accepted.\n");
+            Console.Write("Enter a number: ");
+        }
+
+        Console.WriteLine("\n");
+
+        for (int i = 1; i <= mul; i++) //loops the multiplier
+        {
+            double ans = num * i;
+
+            Console.WriteLine($"{num} * {i} = {ans}");
+        }
+
+    }
+}
