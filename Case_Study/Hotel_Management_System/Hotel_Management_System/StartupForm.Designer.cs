facd1668b0462dbe976bc6cc170b93c21209bd20
Ralph Angelo Tan <ralphangelotan1011@gmail.com>
2024-05-31 16:46:28 +0800

    Added SQLite Database, starting case study development

--- Case_Study/Hotel_Management_System/Hotel_Management_System/StartupForm.Designer.cs
+++ Case_Study/Hotel_Management_System/Hotel_Management_System/StartupForm.Designer.cs
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "StartupForm";
+            SuspendLayout();
+            // 
+            // StartupForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(909, 501);
+            Name = "StartupForm";
+            Text = "StartupForm";
+            ResumeLayout(false);
         }
 
         #endregion
