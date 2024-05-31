8b14d05ed29c5eed7d07cc5de70b269e380f8d56
Ralph Angelo Tan <ralphangelotan1011@gmail.com>
2024-05-31 19:15:04 +0800

    Added Assets folder to handle Logo/s and other images to be used in project.
    Added Visuals on StartupForm

--- Case_Study/Hotel_Management_System/Hotel_Management_System/StartupForm.Designer.cs
+++ Case_Study/Hotel_Management_System/Hotel_Management_System/StartupForm.Designer.cs
@@ -28,18 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StartupForm));
+            btn_Register = new Button();
+            btn_Login = new Button();
+            btnClose = new PictureBox();
+            btnMinimize = new PictureBox();
+            lblCompanyName = new Label();
+            lblDeveloper = new Label();
+            ((System.ComponentModel.ISupportInitialize)btnClose).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)btnMinimize).BeginInit();
             SuspendLayout();
+            // 
+            // btn_Register
+            // 
+            btn_Register.ForeColor = Color.FromArgb(124, 117, 123);
+            btn_Register.Location = new Point(240, 322);
+            btn_Register.Name = "btn_Register";
+            btn_Register.Size = new Size(94, 29);
+            btn_Register.TabIndex = 0;
+            btn_Register.Text = "Register";
+            btn_Register.UseVisualStyleBackColor = true;
+            // 
+            // btn_Login
+            // 
+            btn_Login.ForeColor = Color.FromArgb(124, 117, 123);
+            btn_Login.Location = new Point(574, 322);
+            btn_Login.Name = "btn_Login";
+            btn_Login.Size = new Size(94, 29);
+            btn_Login.TabIndex = 0;
+            btn_Login.Text = "Login";
+            btn_Login.UseVisualStyleBackColor = true;
+            // 
+            // btnClose
+            // 
+            btnClose.Image = (Image)resources.GetObject("btnClose.Image");
+            btnClose.Location = new Point(895, 12);
+            btnClose.Name = "btnClose";
+            btnClose.Size = new Size(35, 35);
+            btnClose.SizeMode = PictureBoxSizeMode.StretchImage;
+            btnClose.TabIndex = 1;
+            btnClose.TabStop = false;
+            btnClose.Click += btnClose_Click;
+            // 
+            // btnMinimize
+            // 
+            btnMinimize.Image = (Image)resources.GetObject("btnMinimize.Image");
+            btnMinimize.Location = new Point(844, 12);
+            btnMinimize.Name = "btnMinimize";
+            btnMinimize.Size = new Size(35, 35);
+            btnMinimize.SizeMode = PictureBoxSizeMode.StretchImage;
+            btnMinimize.TabIndex = 2;
+            btnMinimize.TabStop = false;
+            btnMinimize.Click += btnMinimize_Click;
+            // 
+            // lblCompanyName
+            // 
+            lblCompanyName.Dock = DockStyle.Fill;
+            lblCompanyName.Font = new Font("Times New Roman", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblCompanyName.Location = new Point(0, 0);
+            lblCompanyName.Name = "lblCompanyName";
+            lblCompanyName.Size = new Size(942, 501);
+            lblCompanyName.TabIndex = 3;
+            lblCompanyName.Text = "Company Name";
+            lblCompanyName.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lblDeveloper
+            // 
+            lblDeveloper.AutoSize = true;
+            lblDeveloper.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblDeveloper.Location = new Point(12, 470);
+            lblDeveloper.Name = "lblDeveloper";
+            lblDeveloper.Size = new Size(486, 22);
+            lblDeveloper.TabIndex = 4;
+            lblDeveloper.Text = "Hotel Management System Developed by :  ILPST Solutions";
+            lblDeveloper.TextAlign = ContentAlignment.TopCenter;
             // 
             // StartupForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(909, 501);
+            BackColor = Color.FromArgb(234, 234, 234);
+            ClientSize = new Size(942, 501);
+            Controls.Add(lblDeveloper);
+            Controls.Add(btnMinimize);
+            Controls.Add(btnClose);
+            Controls.Add(btn_Login);
+            Controls.Add(btn_Register);
+            Controls.Add(lblCompanyName);
+            FormBorderStyle = FormBorderStyle.None;
             Name = "StartupForm";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "StartupForm";
+            ((System.ComponentModel.ISupportInitialize)btnClose).EndInit();
+            ((System.ComponentModel.ISupportInitialize)btnMinimize).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btn_Register;
+        private Button btn_Login;
+        private PictureBox btnClose;
+        private PictureBox btnMinimize;
+        private Label lblCompanyName;
+        private Label lblDeveloper;
     }
 }
--- Case_Study/Hotel_Management_System/Hotel_Management_System/StartupForm.cs
+++ Case_Study/Hotel_Management_System/Hotel_Management_System/StartupForm.cs
@@ -5,6 +5,22 @@ namespace Hotel_Management_System
         public StartupForm()
         {
             InitializeComponent();
+            initializeFormAttributes();
+        }
+        public void initializeFormAttributes()
+        {
+            lblCompanyName.Text = "Malake Ltd. Corp. Inc.";
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnMinimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
         }
     }
 }
